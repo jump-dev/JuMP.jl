9bcaff041bd85b06e8aa2d3d7bf0abafd2d6c01b
Iain Dunning <iaindunning@gmail.com>
2013-01-14 18:07:02 -0500

    pmedian jl tweaks

--- test/pmedian.jl
+++ test/pmedian.jl
@@ -5,44 +5,50 @@ require("../src/julp.jl")
 using Julp
 
 function doTest()
-	numFacility = 10
-	numCustomer = 10
-	numLocation = 100
+	numFacility = 100
+	numCustomer = 100
+	numLocation = 10000
 	
 	customerLocations = [randi(numLocation) for a = 1:numCustomer ]
 	
 	tic()
 	m = Model("max")
-
+	
 	# Facility locations
 	s = [ Variable(m,0,1,0,"s$i") for i=1:numLocation ]
 
 	# Aux. variable: x_a,i = 1 iff the closest facility to a is at i
-	x = [ [ Variable(m,0,1,0,"x$a,$i") for a = 1:numCustomer ] for i = 1:numLocation ]
-
+	x = [ Variable(m,0,1,0,"x$a,$i") for i = 1:numLocation, a = 1:numCustomer]
+	
 	# Objective: min distance
-	#m += lpSum( x[(a,i)] * abs(customerLocations[a]-i) for a in range(numCustomer) for i in range(numLocation) )
-	#setObjective(m, @sumExpr(abs(customerLocations[a]-i) * x[i][a] for a = 1:numCustomer, i = 1:numLocation) )
-	vars::Array{Variable,1} = reshape([x[i][a] for a = 1:numCustomer, i = 1:numLocation], (numCustomer*numLocation,))
+	vars::Array{Variable,1} = reshape([x[i,a] for a = 1:numCustomer, i = 1:numLocation], (numCustomer*numLocation,))
 	coef::Array{Float64,1} = reshape([abs(customerLocations[a]-i) for a = 1:numCustomer, i = 1:numLocation], (numCustomer*numLocation,))
 	setObjective(m, AffExpr(vars, coef, 0.))
+	
+	# Constraints
 	
 	for a in 1:numCustomer
 	  # Subject to linking x with s
 	  for i in 1:numLocation
-		addConstraint(m, 1.0*x[i][a] + (-1.0*s[i]) <= 0 )
+		#addConstraint(m, 1.0*x[i,a] + (-1.0*s[i]) <= 0 )
+		addConstraint(m, AffExpr([x[i,a],s[i]],[1.,-1.],0.) <= 0)
 	  end
 	  # Subject to one of x must be 1
-	  addConstraint(m, @sumExpr([1.0*x[i][a] for i = 1:numLocation]) == 1 )
+	  addConstraint(m, @sumExpr([1.0*x[i,a] for i = 1:numLocation]) == 1 )
 	end
 	
 	# Subject to must allocate all facilities
-	addConstraint(m, @sumExpr([1.0*s[i] for i = 1:numLocation]) == 1 )
+	addConstraint(m, @sumExpr([1.0*s[i] for i = 1:numLocation]) == 1 )	
 	toc()
 
 	tic()
 	writeMPS(m,"pmedian_jl.mps")
 	toc()
 end
-println(".")
+
 doTest()
+
+
+#m += lpSum( x[(a,i)] * abs(customerLocations[a]-i) for a in range(numCustomer) for i in range(numLocation) )
+	#setObjective(m, @sumExpr(abs(customerLocations[a]-i) * x[i,a] for a = 1:numCustomer, i = 1:numLocation) )
+	#x = [ [ Variable(m,0,1,0,"x$a,$i") for a = 1:numCustomer ] for i = 1:numLocation ]
