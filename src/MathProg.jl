6e54a2bccebcfeb3036176ea5992575ddbf43f29
Iain Dunning <iaindunning@gmail.com>
2013-06-09 11:53:25 -0700

    removed old sumExpr

--- src/MathProg.jl
+++ src/MathProg.jl
@@ -40,13 +40,6 @@ export
   addToExpression,
   lpSum
 
-
-macro sumExprOld(expr)
-  local coefarr = Expr(:comprehension,convert(Vector{Any},[:(convert(Float64,$(expr.args[1].args[2]))),expr.args[2]]),Any)
-  local vararr = Expr(:comprehension,convert(Vector{Any},[expr.args[1].args[3],expr.args[2]]),Any)
-  esc(:(AffExpr($vararr,$coefarr,0.)))
-end
-
 include("MathProgDict.jl")
 
 ########################################################################
--- test/variable.jl
+++ test/variable.jl
@@ -2,11 +2,20 @@
 # Test coverage for Variable
 
 # Test setters/getters
+# Name
 m = Model("max")
 @defVar(m, 0 <= x <= 2)
 @assert getName(x) == "x"
 setName(x, "x2")
 @assert getName(x) == "x2"
-# is this functionality a good thing?
 setName(x, "")
 @assert getName(x) == "_col1"
+
+
+# Bounds
+@assert getLower(x) == 0
+@assert getUpper(x) == 2
+setLower(x, 1)
+@assert getLower(x) == 1
+setUpper(x, 3)
+@assert getUpper(x) == 3
