027b672ca98e20a829813db708830e92a6ae10b7
Miles Lubin <miles.lubin@gmail.com>
2013-09-21 19:23:02 -0400

    Generalize IndexedVector. Closes #36

--- src/utils.jl
+++ src/utils.jl
@@ -7,14 +7,18 @@ end
 IndexedVector(T::Type,n::Integer) = IndexedVector(zeros(T,n),zeros(Int,n),0)
 
 function addelt{T}(v::IndexedVector{T},i::Integer,val::T)
-    if v.elts[i] == 0 # new index
+    if v.elts[i] == zero(T) # new index
         v.elts[i] = val
         v.nzidx[v.nnz += 1] = i
     else
         v.elts[i] += val
-        if v.elts[i] == 0
-            # set to tiny value
-            v.elts[i] = 1e-50
+        if v.elts[i] == zero(T)
+            # set to tiny value.
+
+            # if two instances of T can sum to zero(T),
+            # type must also implement one().
+            # if not, code will never be reached
+            v.elts[i] = 1e-50*one(T)
         end
     end
 end
