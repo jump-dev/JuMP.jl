72484f3bd46af4ac3f5b1c248170abb3856839ea
Iain Dunning <iaindunning@gmail.com>
2013-12-27 17:23:23 -0500

    Test for user cuts for Gurobi

--- test/callback.jl
+++ test/callback.jl
@@ -1,74 +1,95 @@
 if Pkg.installed("Gurobi") != nothing  
-  using Gurobi
+    using Gurobi
 
-  let
-  	mod = Model(solver=GurobiSolver(LazyConstraints=1))
+    let
+        mod = Model(solver=GurobiSolver(LazyConstraints=1))
 
-  	@defVar(mod, 0 <= x <= 2, Int)
-  	@defVar(mod, 0 <= y <= 2, Int)
-  	@setObjective(mod, Max, y + 0.5x)
-  	function corners(cb)
-  	    x_val = getValue(x)
-  	    y_val = getValue(y)
-      	TOL = 1e-6
-  	    # Check top right
-      	if y_val + x_val > 3 + TOL
-          	@addLazyConstraint(cb, y + x <= 3)
-      	end
-      end
-  	setlazycallback(mod, corners)
-  	solve(mod)
-  	@test_approx_eq_eps getValue(x) 1.0 1e-6
-  	@test_approx_eq_eps getValue(y) 2.0 1e-6
-  end
+        @defVar(mod, 0 <= x <= 2, Int)
+        @defVar(mod, 0 <= y <= 2, Int)
+        @setObjective(mod, Max, y + 0.5x)
+        function corners(cb)
+            x_val = getValue(x)
+            y_val = getValue(y)
+            TOL = 1e-6
+            # Check top right
+            if y_val + x_val > 3 + TOL
+                @addLazyConstraint(cb, y + x <= 3)
+            end
+        end
+        setlazycallback(mod, corners)
+        solve(mod)
+        @test_approx_eq_eps getValue(x) 1.0 1e-6
+        @test_approx_eq_eps getValue(y) 2.0 1e-6
+    end
+    let
+        mod = Model(solver=GurobiSolver(PreCrush=1, Cuts=0, Presolve=0, Heuristics=0.0))
+
+        @defVar(mod, 0 <= x <= 2, Int)
+        @defVar(mod, 0 <= y <= 2, Int)
+        @setObjective(mod, Max, x + 2y)
+        @addConstraint(mod, y + x <= 3.5)
+        function mycutgenerator(cb)
+            x_val = getValue(x)
+            y_val = getValue(y)
+            TOL = 1e-6  
+            # Check top right
+            if y_val + x_val > 3 + TOL
+                @addUserCut(cb, y + x <= 3)
+            end
+        end
+        setcutcallback(mod, mycutgenerator)
+        solve(mod)
+        @test_approx_eq_eps getValue(x) 1.0 1e-6
+        @test_approx_eq_eps getValue(y) 2.0 1e-6
+    end
 end
 
 if Pkg.installed("CPLEXLink") != nothing
-  using CPLEXLink
+    using CPLEXLink
 
-  let
-    mod = Model(solver=CplexSolver())
+    let
+        mod = Model(solver=CplexSolver())
 
-    @defVar(mod, 0 <= x <= 2, Int)
-    @defVar(mod, 0 <= y <= 2, Int)
-    @setObjective(mod, Max, y + 0.5x)
-    function corners(cb)
-        x_val = getValue(x)
-        y_val = getValue(y)
-        TOL = 1e-6
-        # Check top right
-        if y_val + x_val > 3 + TOL
-            @addLazyConstraint(cb, y + x <= 3)
-        end
-      end
-    setlazycallback(mod, corners)
-    solve(mod)
-    @test_approx_eq_eps getValue(x) 1.0 1e-6
-    @test_approx_eq_eps getValue(y) 2.0 1e-6
-  end
+        @defVar(mod, 0 <= x <= 2, Int)
+        @defVar(mod, 0 <= y <= 2, Int)
+        @setObjective(mod, Max, y + 0.5x)
+        function corners(cb)
+                x_val = getValue(x)
+                y_val = getValue(y)
+                TOL = 1e-6
+                # Check top right
+                if y_val + x_val > 3 + TOL
+                        @addLazyConstraint(cb, y + x <= 3)
+                end
+            end
+        setlazycallback(mod, corners)
+        solve(mod)
+        @test_approx_eq_eps getValue(x) 1.0 1e-6
+        @test_approx_eq_eps getValue(y) 2.0 1e-6
+    end
 end
 
 if Pkg.installed("GLPKMathProgInterface") != nothing
-  using GLPKMathProgInterface
+    using GLPKMathProgInterface
 
-  let
-    mod = Model(solver=GLPKSolverMIP())
+    let
+        mod = Model(solver=GLPKSolverMIP())
 
-    @defVar(mod, 0 <= x <= 2, Int)
-    @defVar(mod, 0 <= y <= 2, Int)
-    @setObjective(mod, Max, y + 0.5x)
-    function corners(cb)
-        x_val = getValue(x)
-        y_val = getValue(y)
-        TOL = 1e-6
-        # Check top right
-        if y_val + x_val > 3 + TOL
-            @addLazyConstraint(cb, y + x <= 3)
-        end
-      end
-    setlazycallback(mod, corners)
-    solve(mod)
-    @test_approx_eq_eps getValue(x) 1.0 1e-6
-    @test_approx_eq_eps getValue(y) 2.0 1e-6
-  end
+        @defVar(mod, 0 <= x <= 2, Int)
+        @defVar(mod, 0 <= y <= 2, Int)
+        @setObjective(mod, Max, y + 0.5x)
+        function corners(cb)
+                x_val = getValue(x)
+                y_val = getValue(y)
+                TOL = 1e-6
+                # Check top right
+                if y_val + x_val > 3 + TOL
+                        @addLazyConstraint(cb, y + x <= 3)
+                end
+            end
+        setlazycallback(mod, corners)
+        solve(mod)
+        @test_approx_eq_eps getValue(x) 1.0 1e-6
+        @test_approx_eq_eps getValue(y) 2.0 1e-6
+    end
 end
