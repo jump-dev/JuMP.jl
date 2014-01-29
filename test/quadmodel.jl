e9d50f667f816a11d704b2f3c14f8f128206573e
Iain Dunning <iaindunning@gmail.com>
2014-01-29 16:44:52 -0500

    Quadratic tests looking much better now

--- test/quadmodel.jl
+++ test/quadmodel.jl
@@ -2,38 +2,41 @@
 function qp_test(solvername, solverobj)
     println(string("  Running ", solvername))
     let
+        println("    Test 1")
         modQ = Model(solver=solverobj)
         @defVar(modQ, 1.1*i <= x[i=1:3] <= 2.5*i, Int)
 
-        setObjective(modQ, :Min, 3*x[1]*x[2] + x[2]*x[2] + 9*x[3]*x[1])
+        setObjective(modQ, :Min, 10*x[1]*x[1] + 3*x[1]*x[2] + 5*x[2]*x[2] + 9*x[3]*x[3])
 
         @addConstraint(modQ, x[2] <= 1.7*x[3])
         @addConstraint(modQ, x[2] >= 0.5*x[1])
 
         status = solve(modQ)
         @test status == :Optimal
-        @test_approx_eq modQ.objVal 99.0
-        @test_approx_eq getValue(x) [2.0, 3.0, 4.0]
+        @test_approx_eq_eps modQ.objVal 247.0 1e-6
+        @test_approx_eq_eps getValue(x) [2.0, 3.0, 4.0] 1e-6
     end
 
     # test Maximization sense
     let
+        println("    Test 2")
         modQ = Model(solver=solverobj)
 
         @defVar(modQ, 1.1*i <= x[i=1:3] <= 2.5*i, Int)
 
-        setObjective(modQ, :Max, -3*x[1]*x[2] - x[2]*x[2] - 9*x[3]*x[1])
+        setObjective(modQ, :Max, -10*x[1]*x[1] - 3*x[1]*x[2] - 5*x[2]*x[2] - 9*x[3]*x[3])
 
         @addConstraint(modQ, x[2] <= 1.7*x[3])
         @addConstraint(modQ, x[2] >= 0.5*x[1])
 
         status = solve(modQ)
         @test status == :Optimal
-        @test_approx_eq modQ.objVal -99.0
-        @test_approx_eq getValue(x) [2.0, 3.0, 4.0]
+        @test_approx_eq_eps modQ.objVal -247.0 1e-6
+        @test_approx_eq_eps getValue(x) [2.0, 3.0, 4.0] 1e-6
     end
 
     let
+        println("    Test 3")
         modQ = Model(solver=solverobj)
 
         @defVar(modQ, 0.5 <= x <= 2 )
@@ -53,7 +56,7 @@ end
 
 if Pkg.installed("Gurobi") != nothing  
     using Gurobi
-    qp_test("Gurobi", GurobiSolver())
+    qp_test("Gurobi", GurobiSolver(OutputFlag=0))
 end
 if Pkg.installed("CPLEXLink") != nothing
     using CPLEXLink
