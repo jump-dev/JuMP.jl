09e6eed8c9c7497d7bd238d4e2381ac77330eebb
Miles Lubin <miles.lubin@gmail.com>
2014-04-27 00:31:33 -0400

    simplify to fix crashes

--- src/coloring.jl
+++ src/coloring.jl
@@ -346,8 +346,8 @@ export to_H
 
 # Topological sort using DFS -- copied from Graphs.jl until 0.3 is released...
 
-type MyTopologicalSortVisitor{V} <: AbstractGraphVisitor
-    vertices::Vector{V}
+type MyTopologicalSortVisitor <: AbstractGraphVisitor
+    vertices::Vector{Int}
 
     function MyTopologicalSortVisitor(n::Int)
         vs = Array(Int, 0)
@@ -359,21 +359,20 @@ end
 import Graphs: examine_neighbor!, close_vertex!
 
 
-function examine_neighbor!{V}(visitor::MyTopologicalSortVisitor{V}, u::V, v::V, vcolor::Int, ecolor::Int)
+function examine_neighbor!{V}(visitor::MyTopologicalSortVisitor, u::V, v::V, vcolor::Int, ecolor::Int)
     if vcolor == 1 && ecolor == 0
         throw(ArgumentError("The input graph contains at least one loop."))
     end
 end
 
-function close_vertex!{V}(visitor::MyTopologicalSortVisitor{V}, v::V)
+function close_vertex!{V}(visitor::MyTopologicalSortVisitor, v::V)
     push!(visitor.vertices, v)
 end
 
 function reverse_topological_sort_by_dfs{V}(graph::AbstractGraph{V})
-    @graph_requires graph vertex_list incidence_list vertex_map
 
     cmap = zeros(Int, num_vertices(graph))
-    visitor = MyTopologicalSortVisitor{V}(num_vertices(graph))
+    visitor = MyTopologicalSortVisitor(num_vertices(graph))
 
     for s in vertices(graph)
         if cmap[vertex_index(s, graph)] == 0
