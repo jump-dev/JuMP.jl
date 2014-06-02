2ef17246c96687b07dd98886e5eced8945edbcd4
Joey Huchette <joehuchette@gmail.com>
2014-06-02 13:17:38 -0400

    scratchwork on JuMPArray

---
+++ src/JuMPArray.jl
@@ -0,0 +1,52 @@
+type JuMPArray{T,N::Int,R<:OrdinalRange} <: JuMPContainer
+    innerArray::Array{T,N}
+    name::String
+    indexsets::NTuple{N,R}
+end
+
+function Base.getindex{T,N}(d::JuMPArray{T,N,UnitRange{Int}},indices::Int...)
+    length(indices) == N || error("Wrong number of indices for ",d.name,", expected ",length(d.indexsets))
+    idx = Array(Int, N)
+    for i in 1:N
+        idx[i] = indices[i] - start(d.indexsets[i]) + 1
+    end
+    return d.innerArray(idx)
+end
+
+function Base.getindex{T,N}(d::JuMPArray{T,N,StepRange{Int,Int}},indices::Int...)
+    length(indices) == N || error("Wrong number of indices for ",d.name,", expected ",length(d.indexsets))
+    idx = Array(Int, N)
+    steps  = 0
+    starts = 0
+    for i in 1:N
+        steps  = step(d.indexsets[i])
+        starts = starts(d.indexsets[i])
+        idx[i] = convert(Int,(indices[i]-starts)/steps) + 1
+    end
+    return d.innerArray(idx)
+end
+
+function Base.setindex!{T,N}(d::JuMPArray{T,N,UnitRange{Int}},val::T,indices::Int...)
+    length(indices) == N || error("Wrong number of indices for ",d.name,", expected ",length(d.indexsets))
+    idx = Array(Int, N)
+    for i in 1:N
+        idx[i] = indices[i] - start(d.indexsets[i]) + 1
+    end
+    d.innerArray(idx) = val
+end
+
+function Base.getindex{T,N}(d::JuMPArray{T,N,StepRange{Int,Int}},val::T,indices::Int...)
+    length(indices) == N || error("Wrong number of indices for ",d.name,", expected ",length(d.indexsets))
+    idx = Array(Int, N)
+    steps  = 0
+    starts = 0
+    for i in 1:N
+        steps  = step(d.indexsets[i])
+        starts = starts(d.indexsets[i])
+        idx[i] = convert(Int,(indices[i]-starts)/steps) + 1
+    end
+    d.innerArray(idx) = val
+end
+
+Base.map{T,N,R}(f::Function,d::JuMPArray{T,N,R}) = 
+    JuMPArray{T,N,R}(map(f,d.innerArray), d.name, d.indexsets)
--- src/JuMPDict.jl
+++ src/JuMPDict.jl
@@ -1,7 +1,9 @@
 using Base.Meta
 # multivarate "dictionary" used for collections of variables/constraints
 
-abstract JuMPDict{T}
+abstract JuMPContainer
+
+abstract JuMPDict{T} <: JuMPContainer
 
 typealias IntDict{T} Dict{T,Int}
 
@@ -13,21 +15,31 @@ macro gendict(instancename,T,idxsets...)
     N = length(idxsets)
     typename = symbol(string("JuMPDict",gensym()))
     isrange = Array(Bool,N)
-    offset = Array(Int,N)
     dictnames = Array(Symbol,N)
+    hasstep = false
     for i in 1:N
-        if isexpr(idxsets[i],:(:)) && length(idxsets[i].args) == 2 # don't yet optimize ranges with steps
+        if isexpr(idxsets[i],:(:))
             isrange[i] = true
-            if isa(idxsets[i].args[1],Int)
-                offset[i] = 1 - idxsets[i].args[1]
-            else
-                error("Currently only ranges with integer compile-time starting values are allowed as index sets. $(idxsets[i].args[1]) is not an integer in range $(idxsets[i]).")
+            if length(idxsets[i].args) == 3
+                hasstep = true
             end
         else
             isrange[i] = false
             dictnames[i] = gensym()
         end
     end
+
+    if all(isrange)
+        if hasstep # ...convert UnitRange -> StepRange
+            for i in 1:N
+                push!(idxsets[i].args, idxsets[i].args[2])
+                idxsets[i].args[2] = 1
+            end
+        quote
+            JuMPArray()
+        end
+    end
+
     typecode = :(type $(typename){T} <: JuMPDict{T}; innerArray::Array{T,$N}; name::String;
                         indexsets end)
     builddicts = quote end
@@ -63,7 +75,7 @@ macro gendict(instancename,T,idxsets...)
 
             push!(getidxrhs.args,:(d.($(Expr(:quote,dictnames[i])))[$varname]))
             push!(setidxrhs.args,:(d.($(Expr(:quote,dictnames[i])))[$varname]))
-            push!(maprhs.args,:(d.($(Expr(:quote,dictnames[i])))))
+            push!(   maprhs.args,:(d.($(Expr(:quote,dictnames[i])))))
         end
     end
 
@@ -92,22 +104,22 @@ end
 
 # duck typing approach -- if eltype(innerArray) doesn't support accessor, will fail
 for accessor in (:getValue, :getDual, :getLower, :getUpper)
-    @eval $accessor(x::JuMPDict) = map($accessor,x)
+    @eval $accessor(x::JuMPContainer) = map($accessor,x)
 end
 
 # delegate zero-argument functions
 for f in (:(Base.endof), :(Base.ndims), :(Base.length), :(Base.abs))
-    @eval $f(x::JuMPDict) = $f(x.innerArray)
+    @eval $f(x::JuMPContainer)  = $f(x.innerArray)
 end
 # delegate one-argument functions
 for f in (:(Base.size),)
-    @eval $f(x::JuMPDict,k) = $f(x.innerArray,k)
+    @eval $f(x::JuMPContainer,k) = $f(x.innerArray,k)
 end
 
-(-)(x::JuMPDict,y::Array) = x.innerArray-y
+(-)(x::JuMPContainer,y::Array) = x.innerArray-y
 
-Base.eltype{T}(x::JuMPDict{T}) = T
+Base.eltype{T}(x::JuMPContainer{T}) = T
 
-Base.full(x::JuMPDict) = x
+Base.full(x::JuMPContainer) = x
 
 export @gendict
