11320d5f1a76a55bb3c658a39ce23a852bcfebb4
Miles Lubin <miles.lubin@gmail.com>
2015-03-09 21:39:24 -0400

    Relicense code and tests under mozilla public license version 2 closes #410

--- src/JuMPDict.jl
+++ src/JuMPDict.jl
@@ -1,3 +1,8 @@
+#  Copyright 2015, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 using Base.Meta
 # multivarate "dictionary" used for collections of variables/constraints
 
