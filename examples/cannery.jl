4f5e93a079055e59b0e5695ad64ffb9aae5391fc
Louis Luangkesorn <louis.luangkesorn@icuity.com>
2015-06-16 21:43:09 -0400

    Added MPL lines to all example files

--- examples/cannery.jl
+++ examples/cannery.jl
@@ -1,4 +1,9 @@
 #############################################################################
+#  Copyright 2015, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
 # JuMP implementation of the cannery problem
 # Dantzig, Linear Programming and Extensions,
 # Princeton University Press, Princeton, NJ, 1963.
--- examples/transp.jl
+++ examples/transp.jl
@@ -1,4 +1,9 @@
 #############################################################################
+#  Copyright 2015, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
 # Author: Louis Luangkesorn
 # Date: Jan 30, 2015
 #
