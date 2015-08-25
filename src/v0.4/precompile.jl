a1518eecce34fbdf958baa35e4f40ade6873aac1
Iain Dunning <iaindunning@gmail.com>
2015-08-24 22:44:16 -0400

    Add license header

--- src/v0.4/precompile.jl
+++ src/v0.4/precompile.jl
@@ -1,3 +1,8 @@
+#  Copyright 2015, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 Base.precompile(addToExpression_reorder, (Array{AffExpr,1},Float64,Array{Float64,1}))
 Base.precompile(addToExpression_reorder, (Array{AffExpr,1},Float64,Array{Int,1}))
 Base.precompile(addToExpression_reorder, (Array{AffExpr,1},Float64,Int))
