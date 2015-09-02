d791cf249c083936b4b593c1cacfa9934f032013
Joey Huchette <joehuchette@gmail.com>
2015-09-01 21:37:36 -0400

    loops instead of union types to avoid ambiguities on v0.3

--- src/operators.jl
+++ src/operators.jl
@@ -527,17 +527,25 @@ for (dotop,op) in [(:.+,:+), (:.-,:-), (:.*,:*), (:./,:/)]
         $dotop(lhs::JuMPTypes,rhs::Number) = $op(lhs,rhs)
     end
     for (T1,T2) in [(:JuMPTypes,:Number),(:JuMPTypes,:JuMPTypes),(:Number,:JuMPTypes)]
-        @eval begin
-            $dotop{S<:$T1}(lhs::ArrayOrSparseMat{S},rhs::$T2) = $op(lhs,rhs)
-            function $dotop{S<:$T1,T<:$T2}(lhs::ArrayOrSparseMat{S},rhs::ArrayOrSparseMat{T})
-                size(lhs) == size(rhs) || error("Incompatible dimensions")
-                arr = Array(typeof($op(zero(S),zero(T))), size(rhs))
-                @inbounds for i in eachindex(lhs)
-                    arr[i] = $op(lhs[i], rhs[i])
+        # Need these looks over S1,S2 for v0.3 because Union(Array,SparseMatrix)
+        # gives ambiguity warnings
+        for S1 in (:Array,:SparseMatrixCSC)
+            @eval $dotop{S<:$T1}(lhs::$S1{S},rhs::$T2) = $op(lhs,rhs)
+        end
+        for (S1,S2) in [(:Array,:Array),(:Array,:SparseMatrixCSC),(:SparseMatrixCSC,:Array)]
+            @eval begin
+                function $dotop{S<:$T1,T<:$T2}(lhs::$S1{S},rhs::$S2{T})
+                    size(lhs) == size(rhs) || error("Incompatible dimensions")
+                    arr = Array(typeof($op(zero(S),zero(T))), size(rhs))
+                    @inbounds for i in eachindex(lhs)
+                        arr[i] = $op(lhs[i], rhs[i])
+                    end
+                    arr
                 end
-                arr
             end
-            $dotop{T<:$T2}(lhs::$T1,rhs::ArrayOrSparseMat{T}) = $op(lhs,rhs)
+        end
+        for S2 in (:Array,:SparseMatrixCSC)
+            @eval $dotop{T<:$T2}(lhs::$T1,rhs::$S2{T}) = $op(lhs,rhs)
         end
     end
 end
