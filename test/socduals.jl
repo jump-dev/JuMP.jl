5f7d5e82dacfbbc4d88cdc7f3e0f72f800fb6bec
Emre Yamangil <emreyamangil@gmail.com>
2015-11-20 00:56:34 -0500

    working but weird

--- test/socduals.jl
+++ test/socduals.jl
@@ -5,10 +5,14 @@ m = Model(solver=CplexSolver())
 
 @defVar(m, x[1:5])
 @setObjective(m, Max, x[1] + x[2] + x[3] + x[4] + 2*x[5])
-@addConstraint(m, constr, sum{x[i]^2, i = 2:5} <= x[1])
-@addConstraint(m, constr, x[1] <= 10)
+@addConstraint(m, constrcon1, norm(x[2:5]) <= x[1])
+@addConstraint(m, constrlin1, x[1] <= 5)
+@addConstraint(m, constrlin2, x[1]+2*x[2] - x[3] <= 10)
+@addConstraint(m, constrcon2, norm([2 3;1 1]*x[2:3]-[3;4]) <= x[5] - 2)
+#@addConstraint(m, constrcon2, norm2{x[i], i = 2:5} <= 10)
 
 solve(m)
-@show getDual(constr)
+@show getDual(constrcon1)
+@show getDual(constrcon2)
 @show m.conicconstrDuals
 @show m.linconstrDuals
