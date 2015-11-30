1b386011c6260f084f905f10c75948235c025fc1
Emre Yamangil <emreyamangil@gmail.com>
2015-11-29 22:33:34 -0500

    reduced cost test

--- test/socduals.jl
+++ test/socduals.jl
@@ -54,7 +54,7 @@ facts("LP dual vs SOC dual test / MAX") do
 
 end
 
-facts("LP dual vs SOC dual test / MIN") do
+facts("LP vs SOC dual test / MIN") do
 
     m1 = Model(solver=CplexSolver())
     @defVar(m1, x1[1:2] >= 0)
@@ -80,4 +80,40 @@ facts("LP dual vs SOC dual test / MIN") do
     @fact getDual(c12) --> roughly(getDual(c22),1e-6)
     @fact getDual(c13) --> roughly(getDual(c23),1e-6)
 
+end
+
+facts("LP vs SOC reduced costs test") do
+
+    m1 = Model(solver=CplexSolver())
+    @defVar(m1, x11)
+    @defVar(m1, 1 <= x12 <= 3)
+    @defVar(m1, 0 >= x13 >= -5)
+    @setObjective(m1, Min, -x11 - 2x12 + x13)
+    @addConstraint(m1, c11, 3x11 + x12 <= 4)
+    @addConstraint(m1, c12, x11 + 2x12 >= 1)
+    @addConstraint(m1, c13, -x11 + x12 == 0.5)
+    @addConstraint(m1, c14, x11 + x12 >= x13)
+
+
+    m2 = Model(solver=ECOS.ECOSSolver())
+    @defVar(m2, x21)
+    @defVar(m2, 1 <= x22 <= 3)
+    @defVar(m2, 0 >= x23 >= -5)
+    @setObjective(m2, Min, -x21 - 2x22 + x23)
+    @addConstraint(m2, c21, 3x21 + x22 <= 4)
+    @addConstraint(m2, c22, x21 + 2x22 >= 1)
+    @addConstraint(m2, c23, -x21 + x22 == 0.5)
+    @addConstraint(m2, c24, x21 + x22 >= x23)
+    @addConstraint(m2, norm(x21) <= x22)
+
+    solve(m1)
+    solve(m2)
+    @show getDual(x11)
+    @show getDual(x12)
+    @show getDual(x13)
+    @show getValue(x23)
+
+    @fact getDual(x11) --> roughly(getDual(x21),1e-6)
+    @fact getDual(x12) --> roughly(getDual(x22),1e-6)
+    @fact getDual(x13) --> roughly(getDual(x23),1e-6)
 end
