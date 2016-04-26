4f0f9f41213584d22d9928430233e3955c353c46
Miles Lubin <miles.lubin@gmail.com>
2016-04-26 12:34:32 -0400

    remove misleading comment

--- src/JuMPArray.jl
+++ src/JuMPArray.jl
@@ -3,8 +3,6 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This code is unused for now. See issue #192
-
 immutable JuMPArray{T,N,NT<:NTuple} <: JuMPContainer{T}
     innerArray::Array{T,N}
     indexsets::NT
