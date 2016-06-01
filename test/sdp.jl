999cf56ab8fc65e233b0dbc93f4bfb288cccbf4c
Joey Huchette <joehuchette@gmail.com>
2016-06-01 18:57:04 -0400

    One more test to fail at compile time"

--- test/sdp.jl
+++ test/sdp.jl
@@ -135,7 +135,7 @@ facts("[sdp] Nonsensical SDPs") do
     @fact macroexpand(:(@variable(m, -ones(3,4) <= foo[1:4,1:4] <= ones(4,4), SDP))).head --> :error
     @fact macroexpand(:(@variable(m, -ones(3,4) <= foo[1:4,1:4] <= ones(4,4), Symmetric))).head --> :error
     @fact macroexpand(:(@variable(m, -ones(4,4) <= foo[1:4,1:4] <= ones(4,5), Symmetric))).head --> :error
-    @fact_throws @variable(m, -rand(5,5) <= nonsymmetric[1:5,1:5] <= rand(5,5), Symmetric)
+    @fact macroexpand(:(@variable(m, -rand(5,5) <= nonsymmetric[1:5,1:5] <= rand(5,5), Symmetric))).head --> :error
 end
 
 facts("[sdp] SDP with quadratics") do
