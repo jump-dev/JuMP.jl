4f352adc3e9d13c7d4e1d44dc3ffb64a7fb71e95
Miles Lubin <miles.lubin@gmail.com>
2016-11-24 23:57:53 -0500

    move from FactCheck to Base.Test

--- test/expr.jl
+++ test/expr.jl
@@ -4,76 +4,82 @@
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #############################################################################
 # JuMP
-# An algebraic modelling langauge for Julia
+# An algebraic modeling langauge for Julia
 # See http://github.com/JuliaOpt/JuMP.jl
 #############################################################################
 # test/expr.jl
 # Testing for AffExpr and QuadExpr
 #############################################################################
-using JuMP, FactCheck
+using JuMP
+using Base.Test
 
-facts("[expr] Test expression construction") do
-    maff = Model()
-    @variable(maff, 0 <= x[1:5] <= 1)
-    @variable(maff, 0 <= LongName <= 99)
+@testset "Expressions" begin
 
-    context("AffExpr") do
-        # Test affstr
-        a1 = x[1] + LongName + 5
-        @fact string(a1) --> "x[1] + LongName + 5"
-        # Test like term collection
-        a2 = 2*(x[2] + LongName + x[2]) + 0
-        @fact string(a2) --> "4 x[2] + 2 LongName"
-        # Test appending functionality
-        push!(a1, 5.0, x[2])
-        @fact string(a1) --> "x[1] + LongName + 5 x[2] + 5"
-        append!(a1, a2)
-        @fact string(a1) --> "x[1] + 3 LongName + 9 x[2] + 5"
-        append!(a1, 2.0)
-        @fact string(a1) --> "x[1] + 3 LongName + 9 x[2] + 7"
-        append!(a1, LongName)
-        @fact string(a1) --> "x[1] + 4 LongName + 9 x[2] + 7"
-    end
+    @testset "Test expression construction" begin
+        maff = Model()
+        @variable(maff, 0 <= x[1:5] <= 1)
+        @variable(maff, 0 <= LongName <= 99)
 
-    context("QuadExpr") do
-        # Test string
-        q1 = x[1]*x[2] + 27.2*LongName + 5
-        @fact string(q1) --> "x[1]*x[2] + 27.2 LongName + 5"
-        # Test like term collection
-        q2 = x[1]*x[2] + x[2]*x[1]
-        @fact string(q2) --> "2 x[1]*x[2]"
-    end
-end
-
-facts("[expr] Test getvalue(expr)") do
-    m = Model()
-    @variable(m, 1 <= x[1:3] <= 2)
-    setvalue(x[3], 2)
-    setvalue(x[2], 2)
-    setvalue(x[1], 1)
-    @fact getvalue(x[1]-x[2]+2x[3]-1.0) --> roughly(2.0)
-    @fact getvalue(x[1]*x[1]-2x[2]*x[1]+3x[2]+1) --> roughly(4.0)
-end
-
-facts("[expr] Test expression iterators") do
-    m = Model()
-    @variable(m, x[1:10])
-
-    a1 = 1*x[1] + 2*x[2]
-    k = 1
-    for (coeff,var) in linearterms(a1)
-        if k == 1
-            @fact coeff --> 1
-            @fact var --> exactly(x[1])
-        elseif k == 2
-            @fact coeff --> 2
-            @fact var --> exactly(x[2])
+        @testset "AffExpr" begin
+            # Test affstr
+            a1 = x[1] + LongName + 5
+            @test string(a1) == "x[1] + LongName + 5"
+            # Test like term collection
+            a2 = 2*(x[2] + LongName + x[2]) + 0
+            @test string(a2) == "4 x[2] + 2 LongName"
+            # Test appending functionality
+            push!(a1, 5.0, x[2])
+            @test string(a1) == "x[1] + LongName + 5 x[2] + 5"
+            append!(a1, a2)
+            @test string(a1) == "x[1] + 3 LongName + 9 x[2] + 5"
+            append!(a1, 2.0)
+            @test string(a1) == "x[1] + 3 LongName + 9 x[2] + 7"
+            append!(a1, LongName)
+            @test string(a1) == "x[1] + 4 LongName + 9 x[2] + 7"
         end
-        k += 1
+
+        @testset "QuadExpr" begin
+            # Test string
+            q1 = x[1]*x[2] + 27.2*LongName + 5
+            @test string(q1) == "x[1]*x[2] + 27.2 LongName + 5"
+            # Test like term collection
+            q2 = x[1]*x[2] + x[2]*x[1]
+            @test string(q2) == "2 x[1]*x[2]"
+        end
     end
 
-    a2 = zero(AffExpr)
-    for (coeff, var) in linearterms(a2)
-        @fact coeff --> 0.0  # Shouldn't be called!
+    @testset "Test getvalue(expr)" begin
+        m = Model()
+        @variable(m, 1 <= x[1:3] <= 2)
+        setvalue(x[3], 2)
+        setvalue(x[2], 2)
+        setvalue(x[1], 1)
+        @test isapprox(getvalue(x[1]-x[2]+2x[3]-1.0), 2.0)
+        @test isapprox(getvalue(x[1]*x[1]-2x[2]*x[1]+3x[2]+1), 4.0)
+    end
+
+    @testset "Test expression iterators" begin
+        m = Model()
+        @variable(m, x[1:10])
+
+        a1 = 1*x[1] + 2*x[2]
+        k = 1
+        for (coeff,var) in linearterms(a1)
+            if k == 1
+                @test coeff == 1
+                @test var === x[1]
+            elseif k == 2
+                @test coeff == 2
+                @test var === x[2]
+            end
+            k += 1
+        end
+
+        k = 0
+        a2 = zero(AffExpr)
+        for (coeff, var) in linearterms(a2)
+            k += 1
+        end
+        @test k == 0
     end
 end
--- test/variable.jl
+++ test/variable.jl
@@ -4,132 +4,135 @@
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #############################################################################
 # JuMP
-# An algebraic modelling langauge for Julia
+# An algebraic modeling langauge for Julia
 # See http://github.com/JuliaOpt/JuMP.jl
 #############################################################################
 # test/variable.jl
 # Testing for Variable
 #############################################################################
-using JuMP, FactCheck
+using JuMP
+import JuMP.repl
+using Base.Test
 
-facts("[variable] constructors") do
-    # Constructors
-    mcon = Model()
-    @variable(mcon, nobounds)
-    @variable(mcon, lbonly >= 0)
-    @variable(mcon, ubonly <= 1)
-    @variable(mcon, 0 <= bothb <= 1)
-    @variable(mcon, 0 <= onerange[-5:5] <= 10)
-    @variable(mcon, onerangeub[-7:1] <= 10, Int)
-    @variable(mcon, manyrangelb[0:1,10:20,1:1] >= 2)
-    @fact getlowerbound(manyrangelb[0,15,1]) --> 2
-    s = ["Green","Blue"]
-    @variable(mcon, x[i=-10:10,s] <= 5.5, Int, start=i+1)
-    @fact getupperbound(x[-4,"Green"]) --> 5.5
-    @fact getvalue(x[-3,"Blue"]) --> -2
-    @fact isequal(getvariable(mcon, :lbonly),lbonly) --> true
-    @fact isequal(getvariable(mcon, :ubonly),ubonly) --> true
-    @fact isequal(getvariable(mcon, :onerangeub)[-7],onerangeub[-7]) --> true
-    @variable(mcon, lbonly)
-    @fact_throws ErrorException getvariable(mcon, :lbonly)
-    @fact_throws ErrorException getvariable(mcon, :foo)
-    d = Dict()
-    @variable(mcon, d["bar"][1:10] == 1)
-    @fact getvalue(d["bar"][1]) --> 1
-    @fact typeof(zero(nobounds)) --> AffExpr
-    @fact typeof(one(nobounds)) --> AffExpr
-end
+@testset "Variables" begin
+    @testset "constructors" begin
+        # Constructors
+        mcon = Model()
+        @variable(mcon, nobounds)
+        @variable(mcon, lbonly >= 0)
+        @variable(mcon, ubonly <= 1)
+        @variable(mcon, 0 <= bothb <= 1)
+        @variable(mcon, 0 <= onerange[-5:5] <= 10)
+        @variable(mcon, onerangeub[-7:1] <= 10, Int)
+        @variable(mcon, manyrangelb[0:1,10:20,1:1] >= 2)
+        @test getlowerbound(manyrangelb[0,15,1]) == 2
+        s = ["Green","Blue"]
+        @variable(mcon, x[i=-10:10,s] <= 5.5, Int, start=i+1)
+        @test getupperbound(x[-4,"Green"]) == 5.5
+        @test getvalue(x[-3,"Blue"]) == -2
+        @test isequal(getvariable(mcon, :lbonly),lbonly)
+        @test isequal(getvariable(mcon, :ubonly),ubonly)
+        @test isequal(getvariable(mcon, :onerangeub)[-7],onerangeub[-7])
+        @variable(mcon, lbonly)
+        @test_throws ErrorException getvariable(mcon, :lbonly)
+        @test_throws ErrorException getvariable(mcon, :foo)
+        d = Dict()
+        @variable(mcon, d["bar"][1:10] == 1)
+        @test getvalue(d["bar"][1]) == 1
+        @test typeof(zero(nobounds)) == AffExpr
+        @test typeof(one(nobounds)) == AffExpr
+    end
 
-facts("[variable] get and set bounds") do
-    m = Model()
-    @variable(m, 0 <= x <= 2)
-    @fact getlowerbound(x) --> 0
-    @fact getupperbound(x) --> 2
-    setlowerbound(x, 1)
-    @fact getlowerbound(x) --> 1
-    setupperbound(x, 3)
-    @fact getupperbound(x) --> 3
-    @variable(m, q, Bin)
-    @fact getlowerbound(q) --> 0
-    @fact getupperbound(q) --> 1
-    @variable(m, 0 <= y <= 1, Bin)
-    @fact getlowerbound(y) --> 0
-    @fact getupperbound(y) --> 1
-    @variable(m, fixedvar == 2)
-    @fact getvalue(fixedvar) --> 2
-    @fact getlowerbound(fixedvar) --> 2
-    @fact getupperbound(fixedvar) --> 2
-    setvalue(fixedvar, 5)
-    @fact getvalue(fixedvar) --> 5
-    @fact getlowerbound(fixedvar) --> 5
-    @fact getupperbound(fixedvar) --> 5
-end
+    @testset "get and set bounds" begin
+        m = Model()
+        @variable(m, 0 <= x <= 2)
+        @test getlowerbound(x) == 0
+        @test getupperbound(x) == 2
+        setlowerbound(x, 1)
+        @test getlowerbound(x) == 1
+        setupperbound(x, 3)
+        @test getupperbound(x) == 3
+        @variable(m, q, Bin)
+        @test getlowerbound(q) == 0
+        @test getupperbound(q) == 1
+        @variable(m, 0 <= y <= 1, Bin)
+        @test getlowerbound(y) == 0
+        @test getupperbound(y) == 1
+        @variable(m, fixedvar == 2)
+        @test getvalue(fixedvar) == 2
+        @test getlowerbound(fixedvar) == 2
+        @test getupperbound(fixedvar) == 2
+        setvalue(fixedvar, 5)
+        @test getvalue(fixedvar) == 5
+        @test getlowerbound(fixedvar) == 5
+        @test getupperbound(fixedvar) == 5
+    end
 
-facts("[variable] get and set values") do
-    m = Model()
-    @variable(m, x[1:3])
-    x0 = collect(1:3)
-    setvalue(x, x0)
-    @fact getvalue(x) --> x0
-    @fact getvalue([x[1],x[2],x[3]]) --> x0
+    @testset "get and set values" begin
+        m = Model()
+        @variable(m, x[1:3])
+        x0 = collect(1:3)
+        setvalue(x, x0)
+        @test getvalue(x) == x0
+        @test getvalue([x[1],x[2],x[3]]) == x0
 
-    @variable(m, y[1:3,1:2])
-    @fact_throws DimensionMismatch setvalue(y, collect(1:6))
-end
+        @variable(m, y[1:3,1:2])
+        @test_throws DimensionMismatch setvalue(y, collect(1:6))
+    end
 
-facts("[variable] get and set category") do
-    m = Model()
-    @variable(m, x[1:3])
-    setcategory(x[2], :Int)
-    @fact getcategory(x[3]) --> :Cont
-    @fact getcategory(x[2]) --> :Int
-end
+    @testset "get and set category" begin
+        m = Model()
+        @variable(m, x[1:3])
+        setcategory(x[2], :Int)
+        @test getcategory(x[3]) == :Cont
+        @test getcategory(x[2]) == :Int
+    end
 
-facts("[variable] repeated elements in index set (issue #199)") do
-    repeatmod = Model()
-    s = [:x,:x,:y]
-    @variable(repeatmod, x[s])
-    @fact MathProgBase.numvar(repeatmod) --> 3
-end
+    @testset "repeated elements in index set (issue #199)" begin
+        repeatmod = Model()
+        s = [:x,:x,:y]
+        @variable(repeatmod, x[s])
+        @test MathProgBase.numvar(repeatmod) == 3
+    end
 
-facts("[variable] condition in indexing") do
-    fa = repl[:for_all]
-    inset, dots = repl[:in], repl[:dots]
-    condmod = Model()
-    @variable(condmod, x[i=1:10; iseven(i)])
-    @variable(condmod, y[j=1:10,k=3:2:9; isodd(j+k) && k <= 8])
-    @fact length(x.tupledict) --> 5
-    @fact length(y.tupledict) --> 15
-    @fact string(condmod) --> "Min 0\nSubject to\n x[i] free $fa i $inset {1,2,$dots,9,10} s.t. iseven(i)\n y[j,k] free $fa j $inset {1,2,$dots,9,10}, k $inset {3,5,7,9} s.t. isodd(j + k) and k <= 8\n"
-end
+    @testset "condition in indexing" begin
+        fa = repl[:for_all]
+        inset, dots = repl[:in], repl[:dots]
+        condmod = Model()
+        @variable(condmod, x[i=1:10; iseven(i)])
+        @variable(condmod, y[j=1:10,k=3:2:9; isodd(j+k) && k <= 8])
+        @test length(x.tupledict) == 5
+        @test length(y.tupledict) == 15
+        @test string(condmod) == "Min 0\nSubject to\n x[i] free $fa i $inset {1,2,$dots,9,10} s.t. iseven(i)\n y[j,k] free $fa j $inset {1,2,$dots,9,10}, k $inset {3,5,7,9} s.t. isodd(j + k) and k <= 8\n"
+    end
 
-facts("[variable] @variable returning Array{Variable}") do
-    m = Model()
-    @variable(m, x[1:3,1:4,1:2])
-    @variable(m, y[1:0])
-    @variable(m, z[1:4])
+    @testset "@variable returning Array{Variable}" begin
+        m = Model()
+        @variable(m, x[1:3,1:4,1:2])
+        @variable(m, y[1:0])
+        @variable(m, z[1:4])
 
-    @fact typeof(x) --> Array{Variable,3}
-    @fact typeof(y) --> Array{Variable,1}
-    @fact typeof(z) --> Array{Variable,1}
+        @test typeof(x) == Array{Variable,3}
+        @test typeof(y) == Array{Variable,1}
+        @test typeof(z) == Array{Variable,1}
 
-    @fact typeof(getvalue(x)) --> Array{Float64,3}
-    @fact typeof(getvalue(y)) --> Array{Float64,1}
-    @fact typeof(getvalue(z)) --> Array{Float64,1}
-end
+        @test typeof(getvalue(x)) == Array{Float64,3}
+        @test typeof(getvalue(y)) == Array{Float64,1}
+        @test typeof(getvalue(z)) == Array{Float64,1}
+    end
 
-facts("[variable] getvalue on empty things") do
-    m = Model()
-    @variable(m, x[1:4,  1:0,1:3])   # Array{Variable}
-    @variable(m, y[1:4,  2:1,1:3]) # JuMPArray
-    @variable(m, z[1:4,Set(),1:3]) # JuMPDict
+    @testset "getvalue on empty things" begin
+        m = Model()
+        @variable(m, x[1:4,  1:0,1:3])   # Array{Variable}
+        @variable(m, y[1:4,  2:1,1:3]) # JuMPArray
+        @variable(m, z[1:4,Set(),1:3]) # JuMPDict
 
-    @fact getvalue(x) --> Array(Float64, 4, 0, 3)
-    @fact typeof(getvalue(y)) <: JuMP.JuMPArray{Float64} --> true
-    @fact JuMP.size(getvalue(y)) --> (4,0,3)
-    @fact typeof(getvalue(z)) --> JuMP.JuMPArray{Float64,3,Tuple{UnitRange{Int},Set{Any},UnitRange{Int}}}
-    @fact length(getvalue(z)) --> 0
-end
+        @test getvalue(x) == Array(Float64, 4, 0, 3)
+        @test typeof(getvalue(y)) <: JuMP.JuMPArray{Float64}
+        @test JuMP.size(getvalue(y)) == (4,0,3)
+        @test typeof(getvalue(z)) == JuMP.JuMPArray{Float64,3,Tuple{UnitRange{Int},Set{Any},UnitRange{Int}}}
+        @test length(getvalue(z)) == 0
+    end
 
 # Slices three-dimensional JuMPContainer x[I,J,K]
 # I,J,K can be singletons, ranges, colons, etc.
@@ -151,78 +154,73 @@ function sliceof(x, I, J, K)
         ii += 1
         jj = 1
     end
-    if VERSION >= v"0.5.0-dev+1195"
-        idx = [length(I)==1, length(J)==1, length(K)==1]
-    else
-        idx = [false, false, length(K)==1]
-        if idx[3] == true && length(J) == 1
-            idx[2] = true
-        end
-    end
+    idx = [length(I)==1, length(J)==1, length(K)==1]
     squeeze(y, tuple(find(idx)...))
 end
 
-facts("[variable] Slices of JuMPArray (#684)") do
-    m = Model()
-    @variable(m, x[1:3, 1:4,1:2])
-    @variable(m, y[1:3,-1:2,3:4])
-    @variable(m, z[1:3,-1:2:4,3:4])
-    @variable(m, w[1:3,-1:2,[:red,"blue"]])
+    @testset "Slices of JuMPArray (#684)" begin
+        m = Model()
+        @variable(m, x[1:3, 1:4,1:2])
+        @variable(m, y[1:3,-1:2,3:4])
+        @variable(m, z[1:3,-1:2:4,3:4])
+        @variable(m, w[1:3,-1:2,[:red,"blue"]])
 
-    @fact x[:] --> vec(sliceof(x, 1:3, 1:4, 1:2))
-    @fact x[:,:,:] --> sliceof(x, 1:3, 1:4, 1:2)
-    @fact x[1,:,:] --> sliceof(x, 1, 1:4, 1:2)
-    @fact x[1,:,2] --> sliceof(x, 1, 1:4, 2)
-    @fact_throws x[1,:,3]
-    @fact x[1:2,:,:] --> sliceof(x, 1:2, 1:4, 1:2)
-    @fact x[1:2,:,2] --> sliceof(x, 1:2, 1:4, 2)
-    @fact x[1:2,:,1:2] --> sliceof(x, 1:2, 1:4, 1:2)
-    @fact_throws x[1:2,:,1:3]
+        @test x[:] == vec(sliceof(x, 1:3, 1:4, 1:2))
+        @test x[:,:,:] == sliceof(x, 1:3, 1:4, 1:2)
+        @test x[1,:,:] == sliceof(x, 1, 1:4, 1:2)
+        @test x[1,:,2] == sliceof(x, 1, 1:4, 2)
+        @test_throws BoundsError x[1,:,3]
+        @test x[1:2,:,:] == sliceof(x, 1:2, 1:4, 1:2)
+        @test x[1:2,:,2] == sliceof(x, 1:2, 1:4, 2)
+        @test x[1:2,:,1:2] == sliceof(x, 1:2, 1:4, 1:2)
+        @test_throws BoundsError x[1:2,:,1:3]
 
-    @fact y[:] --> vec(sliceof(y, 1:3, -1:2, 3:4))
-    @fact y[:,:,:] --> sliceof(y, 1:3, -1:2, 3:4)
-    @fact y[1,:,:] --> sliceof(y, 1, -1:2, 3:4)
-    @fact y[1,:,4] --> sliceof(y, 1, -1:2, 4)
-    @fact_throws y[1,:,5]
-    @fact y[1:2,:,:] --> sliceof(y, 1:2, -1:2, 3:4)
-    @fact y[1:2,:,4] --> sliceof(y, 1:2, -1:2, 4)
-    @fact y[1:2,:,3:4] --> sliceof(y, 1:2, -1:2, 3:4)
-    @fact_throws y[1:2,:,1:3]
+        @test y[:] == vec(sliceof(y, 1:3, -1:2, 3:4))
+        @test y[:,:,:] == sliceof(y, 1:3, -1:2, 3:4)
+        @test y[1,:,:] == sliceof(y, 1, -1:2, 3:4)
+        @test y[1,:,4] == sliceof(y, 1, -1:2, 4)
+        @test_throws ErrorException y[1,:,5]
+        @test y[1:2,:,:] == sliceof(y, 1:2, -1:2, 3:4)
+        @test y[1:2,:,4] == sliceof(y, 1:2, -1:2, 4)
+        @test y[1:2,:,3:4] == sliceof(y, 1:2, -1:2, 3:4)
+        @test_throws BoundsError y[1:2,:,1:3]
 
-    @fact z[:] --> vec(sliceof(z, 1:3, -1:2:4, 3:4))
-    @fact z[:,1,:] --> sliceof(z, 1:3, 1, 3:4)
-    @fact z[1,1,:] --> sliceof(z, 1, 1, 3:4)
-    @fact_throws z[:,5,3]
-    @fact z[1:2,1,:] --> sliceof(z, 1:2, 1, 3:4)
-    @fact z[1:2,1,4] --> sliceof(z, 1:2, 1, 4)
-    @fact z[1:2,1,3:4] --> sliceof(z, 1:2, 1, 3:4)
-    @fact_throws z[1:2,1,1:3]
+        @test z[:] == vec(sliceof(z, 1:3, -1:2:4, 3:4))
+        @test z[:,1,:] == sliceof(z, 1:3, 1, 3:4)
+        @test z[1,1,:] == sliceof(z, 1, 1, 3:4)
+        @test_throws ErrorException z[:,5,3]
+        @test z[1:2,1,:] == sliceof(z, 1:2, 1, 3:4)
+        @test z[1:2,1,4] == sliceof(z, 1:2, 1, 4)
+        @test z[1:2,1,3:4] == sliceof(z, 1:2, 1, 3:4)
+        @test_throws BoundsError z[1:2,1,1:3]
 
-    @fact w[:] --> vec(sliceof(w, 1:3, -1:2, [:red,"blue"]))
-    @fact_throws w[:,:,:]
-    @fact w[1,:,"blue"] --> sliceof(w, 1, -1:2, ["blue"])
-    @fact w[1,:,:red] --> sliceof(w, 1, -1:2, [:red])
-    @fact_throws w[1,:,"green"]
-    @fact w[1:2,:,"blue"] --> sliceof(w, 1:2, -1:2, ["blue"])
-    @fact_throws w[1:2,:,[:red,"blue"]]
-end
+        @test w[:] == vec(sliceof(w, 1:3, -1:2, [:red,"blue"]))
+        @test_throws ErrorException w[:,:,:]
+        @test w[1,:,"blue"] == sliceof(w, 1, -1:2, ["blue"])
+        @test w[1,:,:red] == sliceof(w, 1, -1:2, [:red])
+        @test_throws ErrorException w[1,:,"green"]
+        @test w[1:2,:,"blue"] == sliceof(w, 1:2, -1:2, ["blue"])
+        @test_throws ErrorException w[1:2,:,[:red,"blue"]]
+    end
 
-facts("[variable] Can't use end for indexing a JuMPContainer") do
-    m = Model()
-    @variable(m, x[0:2,1:4])
-    @variable(m, y[i=1:4,j=1:4;true])
-    @variable(m, z[0:2])
-    @fact_throws x[end,1]
-    @fact_throws x[end-1]
-    @fact_throws x[0,end-1]
-    @fact_throws y[end,end-1]
-    @fact_throws y[end,1]
-    @fact_throws z[end]
-end
+    @testset "Can't use end for indexing a JuMPContainer" begin
+        m = Model()
+        @variable(m, x[0:2,1:4])
+        @variable(m, y[i=1:4,j=1:4;true])
+        @variable(m, z[0:2])
+        @test_throws ErrorException x[end,1]
+        @test_throws ErrorException x[end-1]
+        @test_throws ErrorException x[0,end-1]
+        @test_throws MethodError y[end,end-1]
+        @test_throws MethodError y[end,1]
+        @test_throws ErrorException z[end]
+    end
 
-facts("[variable] Unsigned dimension lengths") do
-    m = Model()
-    t = UInt(4)
-    @variable(m, x[1:t])
-    @constraintref(y[1:t])
+    @testset "Unsigned dimension lengths" begin
+        m = Model()
+        t = UInt(4)
+        @variable(m, x[1:t])
+        @constraintref(y[1:t])
+        @test MathProgBase.numvar(m) == 4
+    end
 end
