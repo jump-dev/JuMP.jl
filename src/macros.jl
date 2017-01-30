36d1c00a858bfbd0216e099015c9a8c86224d715
Joey Huchette <joehuchette@gmail.com>
2017-01-30 14:40:36 -0500

    Revert "Fix #949"
    This reverts commit 71e169e81b5fe98826c9407ed7f5fba8c299bab0.

--- src/macros.jl
+++ src/macros.jl
@@ -747,7 +747,7 @@ macro expression(args...)
     end
     code = quote
         $code
-        $(refcall) = convert(AffExpr,$newaff)
+        $(refcall) = $newaff
     end
     code = getloopedcode(variable, code, condition, idxvars, idxsets, idxpairs, :AffExpr)
     if m === nothing # deprecated usage
