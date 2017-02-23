3b6bb56208c7074783507833d6a300c3b6c6fa0b
Miles Lubin <miles.lubin@gmail.com>
2017-02-23 10:15:34 -0500

    bump copyright to 2017 and prep 0.16

--- examples/steelT3.jl
+++ examples/steelT3.jl
@@ -1,5 +1,5 @@
 ################################################################################
-#  Copyright 2016, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  Copyright 2017, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
 #  This Source Code Form is subject to the terms of the Mozilla Public
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
