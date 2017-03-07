150ebf44de18fa2529b00ccfeac85ce8164aa559
Joey Huchette <joehuchette@gmail.com>
2017-03-07 17:43:55 -0500

    Rebase onto master

--- test/print.jl
+++ test/print.jl
@@ -676,7 +676,7 @@ end
     @testset "error printing value of variable with weird index set #982" begin
         m = Model()
         @variable(m, x[1:2,1], start=0)
-        io_test(REPLMode, x, "x[i,j] free $for_all i $inset {1,2}, j $inset {1}")
+        io_test(REPLMode, x, "x[i,j] $for_all i $inset {1,2}, j $inset {1}")
         io_test(REPLMode, getvalue(x), """
     x: 2 dimensions:
     [1,:]
