092ffa90acda07a28a16a871479da3f3816a6950
Miles Lubin <miles.lubin@gmail.com>
2017-07-23 22:36:05 +0200

    whitespace

--- test/objective.jl
+++ test/objective.jl
@@ -8,7 +8,7 @@
         @objective(m, Min, 2x)
         @test getobjectivesense(m) == :Min
         @test JuMP.isequal_canonical(getobjective(m, AffExpr), 2x)
-        
+
         @objective(m, Max, x + 3x + 1)
         @test getobjectivesense(m) == :Max
         @test JuMP.isequal_canonical(getobjective(m, AffExpr), 4x + 1)
