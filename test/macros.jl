8a747963d1b2574dba28d72eb1a382c1a3cc2e47
Benoît Legat <benoit.legat@gmail.com>
2018-04-17 11:17:19 +0200

    Move test to macros.jl file

--- test/macros.jl
+++ test/macros.jl
@@ -7,4 +7,12 @@
         ex = @expression(m, sum(i+j+k for ((i,j),k) in d))
         @test ex == 6
     end
+
+    @testset "constructconstraint! on variable" begin
+        m = Model()
+        @variable(m, x)
+        @test JuMP.constructconstraint!(x, MOI.GreaterThan(0.0)) isa JuMP.SingleVariableConstraint{MOI.GreaterThan{Float64}}
+        @test JuMP.constructconstraint!(x, MOI.LessThan(0.0)) isa JuMP.SingleVariableConstraint{MOI.LessThan{Float64}}
+        @test JuMP.constructconstraint!(x, MOI.EqualTo(0)) isa JuMP.SingleVariableConstraint{MOI.EqualTo{Int}}
+    end
 end
