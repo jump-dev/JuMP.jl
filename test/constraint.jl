0a6f1f7a79c5eb45c689a904d312c86de7466c77
Benoît Legat <benoit.legat@gmail.com>
2018-04-30 11:55:27 +0200

    Fix tests of macro errors in Julia v0.7

--- test/constraint.jl
+++ test/constraint.jl
@@ -194,14 +194,14 @@ end
         m = Model()
         @test_throws ErrorException @variable(m, unequal[1:5,1:6], PSD)
         # Some of these errors happen at compile time, so we can't use @test_throws
-        @test macroexpand(:(@variable(m, notone[1:5,2:6], PSD))).head == :error
-        @test macroexpand(:(@variable(m, oneD[1:5], PSD))).head == :error
-        @test macroexpand(:(@variable(m, threeD[1:5,1:5,1:5], PSD))).head == :error
-        @test macroexpand(:(@variable(m, psd[2] <= rand(2,2), PSD))).head == :error
-        @test macroexpand(:(@variable(m, -ones(3,4) <= foo[1:4,1:4] <= ones(4,4), PSD))).head == :error
-        @test macroexpand(:(@variable(m, -ones(3,4) <= foo[1:4,1:4] <= ones(4,4), Symmetric))).head == :error
-        @test macroexpand(:(@variable(m, -ones(4,4) <= foo[1:4,1:4] <= ones(4,5), Symmetric))).head == :error
-        @test macroexpand(:(@variable(m, -rand(5,5) <= nonsymmetric[1:5,1:5] <= rand(5,5), Symmetric))).head == :error
+        @test_macro_throws @variable(m, notone[1:5,2:6], PSD)
+        @test_macro_throws @variable(m, oneD[1:5], PSD)
+        @test_macro_throws @variable(m, threeD[1:5,1:5,1:5], PSD)
+        @test_macro_throws @variable(m, psd[2] <= rand(2,2), PSD)
+        @test_macro_throws @variable(m, -ones(3,4) <= foo[1:4,1:4] <= ones(4,4), PSD)
+        @test_macro_throws @variable(m, -ones(3,4) <= foo[1:4,1:4] <= ones(4,4), Symmetric)
+        @test_macro_throws @variable(m, -ones(4,4) <= foo[1:4,1:4] <= ones(4,5), Symmetric)
+        @test_macro_throws @variable(m, -rand(5,5) <= nonsymmetric[1:5,1:5] <= rand(5,5), Symmetric)
     end
 
     @testset "[macros] sum(generator)" begin
