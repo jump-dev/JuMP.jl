d46b1552176c88214f766fffcf1a5660a6738245
Benoît Legat <benoit.legat@gmail.com>
2018-05-01 14:50:52 +0200

    Fix tests of macro errors in Julia v0.7 (#1256)
    * Fix tests of macro errors in Julia v0.7
    
    * Fix test_macro_throws on Julia v0.6
    
    * More consistent test_macro_throws

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,6 +18,12 @@ const MOI = MathOptInterface
 const MOIT = MOI.Test
 const MOIU = MOI.Utilities
 
+# Test that the macro call `m` throws an error exception during pre-compilation
+macro test_macro_throws(errortype, m)
+    # See https://discourse.julialang.org/t/test-throws-with-macros-after-pr-23533/5878
+    :(@test_throws $errortype try @eval $m catch err; throw(err.error) end)
+end
+
 include("derivatives.jl")
 include("derivatives_coloring.jl")
 
