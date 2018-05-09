1ce43670e243c335640a8894269a153add27e38f
Benoît Legat <benoit.legat@gmail.com>
2018-05-09 13:46:01 +0200

    Fix consistency of exponent

--- src/parseexpr.jl
+++ src/parseexpr.jl
@@ -497,9 +497,9 @@ function parseExpr(x, aff::Symbol, lcoeffs::Vector, rcoeffs::Vector, newaff::Sym
                 push!(blk.args, :($newaff = addtoexpr_reorder($aff, $(Expr(:call,:*,lcoeffs...,newaff_,newaff_,rcoeffs...)))))
                 return newaff, blk
             elseif x.args[3] == 1
-                return parseExpr(x.args[2], aff, lcoeffs, rcoeffs)
+                return parseExpr(:(QuadExpr($(x.args[2]))), aff, lcoeffs, rcoeffs)
             elseif x.args[3] == 0
-                return parseExpr(1, aff, lcoeffs, rcoeffs)
+                return parseExpr(:(QuadExpr(1)), aff, lcoeffs, rcoeffs)
             else
                 blk = Expr(:block)
                 s = gensym()
