348f8826dd2a6c36523f09bf5a9ef4b408a5683c
ExpandingMan <savastio@protonmail.com>
2018-08-22 11:09:12 -0400

    attempt to fix method error in custom type test

--- test/operator.jl
+++ test/operator.jl
@@ -38,14 +38,14 @@ Base.transpose(t::MySumType) = MySumType(t.a)
 *(t1::MyType{S}, t2::T) where {S, T} = MyType(t1.a*t2)
 *(t1::S, t2::MyType{T}) where {S, T} = MyType(t1*t2.a)
 *(t1::MyType{S}, t2::MyType{T}) where {S, T} = MyType(t1.a*t2.a)
+Base.convert(::Type{MySumType{T}}, t::MyType{T}) where {T} = MySumType(t.a)
 if VERSION ≥ v"0.7-"
     Base.adjoint(t::MyType) = t
     Base.adjoint(t::MySumType) = t
-    Base.convert(::Type{MySumType{T}}, t::MyType{T}) where {T} = MySumType(t.a)
 end
 
 
-#@testset "Operator overloads" begin
+@testset "Operator overloads" begin
 
     _lt(x,y) = (x.col < y.col)
     function sort_expr!(x::AffExpr)
@@ -899,4 +899,4 @@ end
             @test z[i].a == y[i].a == a
         end
     end
-#end
+end
