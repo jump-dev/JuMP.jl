71a40cee095df3234e3f838da9cdbdbe4b9dc251
Benoît Legat <benoit.legat@gmail.com>
2018-09-19 09:40:02 +0200

    Add copyright

--- src/constraints.jl
+++ src/constraints.jl
@@ -1,6 +1,13 @@
+#  Copyright 2017, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #############################################################################
-# AbstractShape
-# Abstract base type for the shape of constraints
+# JuMP
+# An algebraic modeling language for Julia
+# See http://github.com/JuliaOpt/JuMP.jl
+#############################################################################
+
 """
     AbstractShape
 
