4a7f56e07c938cd2030a8622a1ecfa13d62e0350
Oscar Dowson <odow@users.noreply.github.com>
2018-11-08 08:30:35 -0600

    [ci skip] update JuMP.backend docstring.

--- src/JuMP.jl
+++ src/JuMP.jl
@@ -292,11 +292,11 @@ Return the lower-level MathOptInterface model that sits underneath JuMP. This
 model depends on which operating mode JuMP is in (manual, automatic, or direct),
 and whether there are any bridges in the model.
 
-If JuMP is in direct mode (i.e., the model was created using `direct_model`),
+If JuMP is in direct mode (i.e., the model was created using [`JuMP.direct_model`](@ref)),
 the backend with be the optimizer passed to `direct_model`. If JuMP is in manual
 or automatic mode, the backend will either be a
-[@ref](`MOI.Utilities.CachingOptimizer`) or a
-[@ref](`MOI.Bridges.LazyBridgeOptimizer`).
+[`MOI.Utilities.CachingOptimizer`](@ref) or a
+[`MOI.Bridges.LazyBridgeOptimizer`](@ref).
 
 This function should only be used by advanced users looking to access low-level
 MathOptInterface or solver-specific functionality.
