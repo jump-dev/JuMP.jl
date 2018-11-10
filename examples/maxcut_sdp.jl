67133a8ab0b23a930e2a6f28b72187535e1b65e8
Iain Dunning <iaindunning@gmail.com>
2018-11-10 17:19:33 -0500

    Make SDP example work, move it, close #1547 (#1599)
    * Make SDP example work
    
    * Move SDP example, close #1547
    
    * Use value, license

---
+++ examples/maxcut_sdp.jl
@@ -0,0 +1,147 @@
+#  Copyright 2017, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#############################################################################
+# JuMP
+# An algebraic modeling langauge for Julia
+# See http://github.com/JuliaOpt/JuMP.jl
+#############################################################################
+# maxcut_sdp.jl
+#
+# Demonstrates applying the SDP relaxation to the classic MAXCUT problem.
+#############################################################################
+
+using LinearAlgebra
+import Random
+
+using JuMP
+import SCS
+
+
+"""
+    solve_maxcut_sdp(num_vertex, weights)
+
+Solves a semidefinite programming relaxation of the MAXCUT graph problem:
+
+    max   0.25 * L•X
+    s.t.  diag(X) == e
+          X ≽ 0
+
+Where `L` is the weighted graph Laplacian. Uses this relaxation to generate
+a solution to the original MAXCUT problem using the method from the paper:
+
+    Goemans, M. X., & Williamson, D. P. (1995).
+    Improved approximation algorithms for maximum cut and satisfiability
+    problems using semidefinite programming.
+    Journal of the ACM (JACM), 42(6), 1115-1145.
+"""
+function solve_maxcut_sdp(num_vertex, weights)
+    # Calculate the (weighted) Lapacian of the graph: L = D - W.
+    laplacian = diagm(0 => weights * ones(num_vertex)) - weights
+
+    # Solve the SDP relaxation
+    model = Model(with_optimizer(SCS.Optimizer))
+    @variable(model, X[1:num_vertex, 1:num_vertex], PSD)
+    @objective(model, Max, 1/4 * dot(laplacian, X))
+    @constraint(model, diag(X) .== 1)
+    JuMP.optimize!(model)
+ 
+    # Compute the Cholesky factorization of X, i.e., X = V^T V.
+    opt_X = Hermitian(JuMP.value.(X), :U)  # Tell Julia its PSD.
+    factorization = cholesky(opt_X, Val(true); check = false)
+    V = (factorization.P * factorization.L)'
+
+    # Normalize columns.
+    for i in 1:num_vertex
+        V[:, i] ./= norm(V[:, i])
+    end
+
+    # Generate random vector on unit sphere.
+    Random.seed!(num_vertex)
+    r = rand(num_vertex)
+    r /= norm(r)
+    
+    # Iterate over vertices, and assign each vertex to a side of cut.
+    cut = ones(num_vertex)
+    for i in 1:num_vertex
+        if dot(r, V[:, i]) <= 0
+            cut[i] = -1
+        end
+    end
+
+    return cut, 0.25 * sum(laplacian .* (cut * cut'))
+end
+
+function test1()
+    #   [1] --- 5 --- [2]
+    #
+    # Solution:
+    #  S  = {1}
+    #  S' = {2}
+    n = 2
+    W = [0.0 5.0;
+         5.0 0.0]
+    cut, cutval = solve_maxcut_sdp(n, W)
+
+    @assert cut[1] != cut[2]
+
+    println("Solution for Graph 1 = $cutval")
+    println(cut)
+end
+
+function test2()
+    #   [1] --- 5 --- [2]
+    #    |  \          |
+    #    |    \        |
+    #    7      6      1
+    #    |        \    |
+    #    |          \  |
+    #   [3] --- 1 --- [4]
+    #
+    # Solution:
+    #  S  = {1}
+    #  S' = {2,3,4}
+    n = 4
+    W = [0.0 5.0 7.0 6.0;
+         5.0 0.0 0.0 1.0;
+         7.0 0.0 0.0 1.0;
+         6.0 1.0 1.0 0.0]
+    cut, cutval = solve_maxcut_sdp(n, W)
+    @assert cut[2] != cut[1]
+    @assert cut[2] == cut[3]
+    @assert cut[2] == cut[4]
+
+    println("Solution for Graph 2 = $cutval")
+    println(cut)
+end
+
+function test3()
+    #   [1] --- 1 --- [2]
+    #    |             |
+    #    |             |
+    #    5             9
+    #    |             |
+    #    |             |
+    #   [3] --- 2 --- [4]
+    #
+    # Solution:
+    #  S  = {2,3}
+    #  S' = {1,4}
+    n = 4
+    W = [0.0 1.0 5.0 0.0;
+         1.0 0.0 0.0 9.0;
+         5.0 0.0 0.0 2.0;
+         0.0 9.0 2.0 0.0]
+    cut, cutval = solve_maxcut_sdp(n, W)
+    @assert cut[1] == cut[4]
+    @assert cut[2] == cut[3]
+    @assert cut[1] != cut[2]
+
+    println("Solution for Graph 3 = $cutval")
+    println(cut)
+end
+
+test1()
+test2()
+test3()
