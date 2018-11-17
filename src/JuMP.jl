564522c2a76ef7ef19deee37a88958980e239bf0
Benoît Legat <benoit.legat@gmail.com>
2018-11-17 19:09:04 +0100

    Improve doc of bridge_constraints

--- src/JuMP.jl
+++ src/JuMP.jl
@@ -297,8 +297,9 @@ end
 """
     bridge_constraints(model::Model)
 
-Return a `Bool` indicating whether the model `model` is in manual or automatic
-mode, the optimizer is set and unsupported constraints are automatically bridged
+When in direct mode, return `false`.
+When in manual or automatic mode, return a `Bool` indicating whether the
+optimizer is set and unsupported constraints are automatically bridged
 to equivalent supported constraints when an appropriate transformation is
 available.
 """
