aecff54187da27a6a3bb9cf6be17467856deba8a
Benoît Legat <benoit.legat@gmail.com>
2018-12-10 11:45:27 +0100

    🔥 Remove JuMPArray.jl

--- src/containers.jl
+++ src/containers.jl
@@ -4,7 +4,6 @@
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 include("Containers/Containers.jl")
-include("JuMPArray.jl")
 
 validarrayindexset(::Base.OneTo) = true
 validarrayindexset(r::UnitRange) = first(r) == 1
