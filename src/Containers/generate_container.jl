a9016a04f3e12e0b7bc528757b38039bbcf95915
Benoît Legat <benoit.legat@gmail.com>
2018-12-12 13:06:58 +0100

    Add TODO

--- src/Containers/generate_container.jl
+++ src/Containers/generate_container.jl
@@ -3,6 +3,7 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# TODO add doc
 depends_on(ex::Expr,s::Symbol) = any(a->depends_on(a,s), ex.args)
 depends_on(ex::Symbol,s::Symbol) = (ex == s)
 depends_on(ex,s::Symbol) = false
