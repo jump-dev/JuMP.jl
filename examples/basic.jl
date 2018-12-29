d2d1aedbacb81b5462e3557fabf1f815133157ba
odow <o.dowson@gmail.com>
2018-12-29 16:05:08 -0600

    Tidy basic.jl

--- examples/basic.jl
+++ examples/basic.jl
@@ -7,29 +7,47 @@
 # An algebraic modeling langauge for Julia
 # See http://github.com/JuliaOpt/JuMP.jl
 #############################################################################
-# basic.jl
-#
-# Solves a simple LP:
-# max 5x + 3y
-#  st 1x + 5y <= 3
-#      0 <= x <= 2
-#      0 <= y <= 30
-#############################################################################
 
-using JuMP, Clp
+using JuMP, GLPK, Test
 
-m = Model(with_optimizer(Clp.Optimizer))
+#=
+    example_basic([; verbose = true])
 
-@variable(m, 0 <= x <= 2)
-@variable(m, 0 <= y <= 30)
+Formulate and solve a simple LP:
+    max 5x + 3y
+     st 1x + 5y <= 3
+         0 <= x <= 2
+         0 <= y <= 30
 
-@objective(m, Max, 5x + 3y)
-@constraint(m, 1x + 5y <= 3.0)
+If `verbose = true`, print the model and the solution.
+=#
 
-print(m)
+function example_basic(; verbose = true)
+    model = Model(with_optimizer(GLPK.Optimizer))
 
-JuMP.optimize!(m)
+    @variable(model, 0 <= x <= 2)
+    @variable(model, 0 <= y <= 30)
 
-println("Objective value: ", JuMP.objective_value(m))
-println("x = ", JuMP.value(x))
-println("y = ", JuMP.value(y))
+    @objective(model, Max, 5x + 3y)
+    @constraint(model, 1x + 5y <= 3.0)
+
+    if verbose
+        print(model)
+    end
+
+    JuMP.optimize!(model)
+
+    obj_value = JuMP.objective_value(model)
+    x_value = JuMP.value(x)
+    y_value = JuMP.value(y)
+
+    if verbose
+        println("Objective value: ", obj_value)
+        println("x = ", x_value)
+        println("y = ", y_value)
+    end
+
+    @test obj_value ≈ 10.6
+    @test x_value ≈ 2
+    @test y_value ≈ 0.2
+end
