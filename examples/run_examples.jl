157d2be2840cf1dfe724eb2c462dac6cfb234369
odow <o.dowson@gmail.com>
2018-12-29 16:07:09 -0600

    Add CI tests for examples

---
+++ examples/run_examples.jl
@@ -0,0 +1,27 @@
+#  Copyright 2018, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public License,
+#  v. 2.0. If a copy of the MPL was not distributed with this file, You can
+#  obtain one at http://mozilla.org/MPL/2.0/.
+
+#=
+    This script runs all the JuMP examples.
+    julia --project=examples run_examples.jl
+=#
+
+using Test
+
+const EXAMPLES = [
+    "basic.jl",
+    "cannery.jl",
+    "optcontrol.jl",
+    "rosenbrock.jl"
+]
+
+@testset "run_examples.jl" begin
+    for example in EXAMPLES
+        @testset "$(example)" begin
+            example_function = include(example)
+            example_function(verbose = false)
+        end
+    end
+end
