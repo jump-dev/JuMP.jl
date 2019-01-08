fc135ef714cc9084ae755e0580f724367bbbf86c
Benoît Legat <benoit.legat@gmail.com>
2019-01-08 10:44:26 +0100

    📄 Add license

--- test/nonnegative_bridge.jl
+++ test/nonnegative_bridge.jl
@@ -1,3 +1,13 @@
+#  Copyright 2017, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#############################################################################
+# JuMP
+# An algebraic modeling language for Julia
+# See http://github.com/JuliaOpt/JuMP.jl
+#############################################################################
+
 using MathOptInterface
 const MOI = MathOptInterface
 const MOIB = MOI.Bridges
