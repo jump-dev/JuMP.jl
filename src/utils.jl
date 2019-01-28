c36b97a8ab4a8c748989bfbce1f1911f19017278
Oscar Dowson <odow@users.noreply.github.com>
2019-01-28 08:46:39 -0600

    [style] sanitize exports from utils.jl (#1814)

--- src/utils.jl
+++ src/utils.jl
@@ -3,41 +3,49 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# lightweight unsafe view for vectors
-# it seems that the only way to avoid triggering
-# allocations is to have only bitstype fields, so
-# we store a pointer.
-struct VectorView{T} <: DenseVector{T}
+# Lightweight unsafe view for vectors. It seems that the only way to avoid
+# triggering allocations is to have only bitstype fields, so we store a pointer.
+struct _VectorView{T} <: DenseVector{T}
     offset::Int
     len::Int
     ptr::Ptr{T}
 end
 
-Base.getindex(v::VectorView,idx::Integer) = unsafe_load(v.ptr, idx+v.offset)
-Base.setindex!(v::VectorView,value,idx::Integer) = unsafe_store!(v.ptr,value,idx+v.offset)
-function Base.setindex!(v::VectorView{T},value::T,idx::Vector{Int}) where T
+Base.getindex(v::_VectorView, idx::Integer) = unsafe_load(v.ptr, idx + v.offset)
+
+function Base.setindex!(v::_VectorView, value, idx::Integer)
+    unsafe_store!(v.ptr, value, idx + v.offset)
+    return v
+end
+
+function Base.setindex!(v::_VectorView{T}, value::T, idx::Vector{Int}) where {T}
     for i in idx
         v[i] = value
     end
+    return v
 end
-Base.length(v::VectorView) = v.len
-function Base.fill!(v::VectorView{T},value) where T
-    val = convert(T,value)
+
+Base.length(v::_VectorView) = v.len
+
+function Base.fill!(v::_VectorView{T}, value) where {T}
+    val = convert(T, value)
     for i in 1:length(v)
         v[i] = val
     end
-    nothing
+    return v
 end
-function rmul!(v::VectorView{T},value::T) where T<:Number
+
+function _rmul!(v::_VectorView{T}, value::T) where {T<:Number}
     for i in 1:length(v)
         v[i] *= value
     end
-    nothing
+    return
 end
-function reinterpret_unsafe(::Type{T}, x::Vector{R}) where {T, R}
+
+function _reinterpret_unsafe(::Type{T}, x::Vector{R}) where {T, R}
     # how many T's fit into x?
     @assert isbitstype(T) && isbitstype(R)
     len = length(x) * sizeof(R)
     p = reinterpret(Ptr{T}, pointer(x))
-    return VectorView(0, div(len, sizeof(T)), p)
+    return _VectorView(0, div(len, sizeof(T)), p)
 end
