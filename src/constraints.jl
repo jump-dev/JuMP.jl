8f7f9f669861c32f4c3811d843d647ecd7eca279
Miles Lubin <mlubin@google.com>
2019-01-29 21:41:23 -0500

    typo fix

--- src/constraints.jl
+++ src/constraints.jl
@@ -274,7 +274,7 @@ end
 
 An abstract base type for all constraint types. `AbstractConstraint`s store the
 function and set directly, unlike [`ConstraintRef`](@ref)s that are merely
-references to constraints stored in a model. `AbstractConstraint` do not need
+references to constraints stored in a model. `AbstractConstraint`s do not need
 to be attached to a model.
 """
 abstract type AbstractConstraint end
