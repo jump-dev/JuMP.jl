291e06784d6a8904d9bb88cf2b14df17d426fd6d
Benoît Legat <benoit.legat@gmail.com>
2019-02-07 18:16:26 +0100

    Sanitize print

--- src/print.jl
+++ src/print.jl
@@ -29,25 +29,25 @@ abstract type IJuliaMode <: PrintMode end
 
 # Whether something is zero or not for the purposes of printing it
 # oneunit is useful e.g. if coef is a Unitful quantity.
-is_zero_for_printing(coef) = abs(coef) < 1e-10 * oneunit(coef)
+_is_zero_for_printing(coef) = abs(coef) < 1e-10 * oneunit(coef)
 # Whether something is one or not for the purposes of printing it.
-is_one_for_printing(coef) = is_zero_for_printing(abs(coef) - oneunit(coef))
-sign_string(coef) = coef < zero(coef) ? " - " : " + "
+_is_one_for_printing(coef) = _is_zero_for_printing(abs(coef) - oneunit(coef))
+_sign_string(coef) = coef < zero(coef) ? " - " : " + "
 
 # Helper function that rounds carefully for the purposes of printing
 # e.g.   5.3  =>  5.3
 #        1.0  =>  1
-function string_round(f::Float64)
+function _string_round(f::Float64)
     iszero(f) && return "0" # strip sign off zero
     str = string(f)
     length(str) >= 2 && str[end-1:end] == ".0" ? str[1:end-2] : str
 end
-string_round(f) = string(f)
+_string_round(f) = string(f)
 
 # REPL-specific symbols
 # Anything here: https://en.wikipedia.org/wiki/Windows-1252
 # should probably work fine on Windows
-function math_symbol(::Type{REPLMode}, name::Symbol)
+function _math_symbol(::Type{REPLMode}, name::Symbol)
     if name == :leq
         return Sys.iswindows() ? "<=" : "≤"
     elseif name == :geq
@@ -94,7 +94,7 @@ function math_symbol(::Type{REPLMode}, name::Symbol)
 end
 
 # IJulia-specific symbols.
-function math_symbol(::Type{IJuliaMode}, name::Symbol)
+function _math_symbol(::Type{IJuliaMode}, name::Symbol)
     if name == :leq
         return "\\leq"
     elseif name == :geq
@@ -140,10 +140,10 @@ function math_symbol(::Type{IJuliaMode}, name::Symbol)
     end
 end
 
-wrap_in_math_mode(str) = "\$\$ $str \$\$"
-wrap_in_inline_math_mode(str) = "\$ $str \$"
+_wrap_in_math_mode(str) = "\$\$ $str \$\$"
+_wrap_in_inline_math_mode(str) = "\$ $str \$"
 
-plural(n) = (n==1 ? "" : "s")
+plural(n) = (n == 1 ? "" : "s")
 
 #------------------------------------------------------------------------
 ## Model
@@ -193,7 +193,7 @@ function Base.print(io::IO, model::AbstractModel)
     print(io, model_string(REPLMode, model))
 end
 function Base.show(io::IO, ::MIME"text/latex", model::AbstractModel)
-    print(io, wrap_in_math_mode(model_string(IJuliaMode, model)))
+    print(io, _wrap_in_math_mode(model_string(IJuliaMode, model)))
 end
 
 # An `AbstractModel` subtype should implement `objective_function_string` and
@@ -261,7 +261,7 @@ end
 #------------------------------------------------------------------------
 Base.show(io::IO, v::AbstractVariableRef) = print(io, var_string(REPLMode, v))
 function Base.show(io::IO, ::MIME"text/latex", v::AbstractVariableRef)
-    print(io, wrap_in_math_mode(var_string(IJuliaMode, v)))
+    print(io, _wrap_in_math_mode(var_string(IJuliaMode, v)))
 end
 function var_string(::Type{REPLMode}, v::AbstractVariableRef)
     var_name = name(v)
@@ -283,24 +283,24 @@ end
 
 Base.show(io::IO, a::GenericAffExpr) = print(io, aff_string(REPLMode,a))
 function Base.show(io::IO, ::MIME"text/latex", a::GenericAffExpr)
-    print(io, wrap_in_math_mode(aff_string(IJuliaMode, a)))
+    print(io, _wrap_in_math_mode(aff_string(IJuliaMode, a)))
 end
 
 function aff_string(mode, a::GenericAffExpr, show_constant=true)
     # If the expression is empty, return the constant (or 0)
     if length(linear_terms(a)) == 0
-        return show_constant ? string_round(a.constant) : "0"
+        return show_constant ? _string_round(a.constant) : "0"
     end
 
     term_str = Array{String}(undef, 2 * length(linear_terms(a)))
     elm = 1
     # For each non-zero for this model
     for (coef, var) in linear_terms(a)
-        is_zero_for_printing(coef) && continue  # e.g. x - x
+        _is_zero_for_printing(coef) && continue  # e.g. x - x
 
-        pre = is_one_for_printing(coef) ? "" : string_round(abs(coef)) * " "
+        pre = _is_one_for_printing(coef) ? "" : _string_round(abs(coef)) * " "
 
-        term_str[2 * elm - 1] = sign_string(coef)
+        term_str[2 * elm - 1] = _sign_string(coef)
         term_str[2 * elm] = string(pre, var_string(mode, var))
         elm += 1
     end
@@ -308,14 +308,14 @@ function aff_string(mode, a::GenericAffExpr, show_constant=true)
     if elm == 1
         # Will happen with cancellation of all terms
         # We should just return the constant, if its desired
-        return show_constant ? string_round(a.constant) : "0"
+        return show_constant ? _string_round(a.constant) : "0"
     else
         # Correction for very first term - don't want a " + "/" - "
         term_str[1] = (term_str[1] == " - ") ? "-" : ""
         ret = join(term_str[1 : 2 * (elm - 1)])
-        if !is_zero_for_printing(a.constant) && show_constant
-            ret = string(ret, sign_string(a.constant),
-                         string_round(abs(a.constant)))
+        if !_is_zero_for_printing(a.constant) && show_constant
+            ret = string(ret, _sign_string(a.constant),
+                         _string_round(abs(a.constant)))
         end
         return ret
     end
@@ -326,7 +326,7 @@ end
 #------------------------------------------------------------------------
 Base.show(io::IO, q::GenericQuadExpr) = print(io, quad_string(REPLMode,q))
 function Base.show(io::IO, ::MIME"text/latex", q::GenericQuadExpr)
-    print(io, wrap_in_math_mode(quad_string(IJuliaMode, q)))
+    print(io, _wrap_in_math_mode(quad_string(IJuliaMode, q)))
 end
 
 function quad_string(mode, q::GenericQuadExpr)
@@ -337,19 +337,19 @@ function quad_string(mode, q::GenericQuadExpr)
     elm = 1
     if length(term_str) > 0
         for (coef, var1, var2) in quad_terms(q)
-            is_zero_for_printing(coef) && continue  # e.g. x - x
+            _is_zero_for_printing(coef) && continue  # e.g. x - x
 
-            pre = is_one_for_printing(coef) ? "" : string_round(abs(coef)) * " "
+            pre = _is_one_for_printing(coef) ? "" : _string_round(abs(coef)) * " "
 
             x = var_string(mode,var1)
             y = var_string(mode,var2)
 
-            term_str[2 * elm - 1] = sign_string(coef)
+            term_str[2 * elm - 1] = _sign_string(coef)
             term_str[2 * elm] = "$pre$x"
             if x == y
-                term_str[2 * elm] *= math_symbol(mode, :sq)
+                term_str[2 * elm] *= _math_symbol(mode, :sq)
             else
-                term_str[2 * elm] *= string(math_symbol(mode, :times), y)
+                term_str[2 * elm] *= string(_math_symbol(mode, :times), y)
             end
             if elm == 1
                 # Correction for first term as there is no space
@@ -484,21 +484,21 @@ function function_string(print_mode, constraint::AbstractConstraint)
 end
 
 function in_set_string(print_mode, set::MOI.LessThan)
-    return string(math_symbol(print_mode, :leq), " ", set.upper)
+    return string(_math_symbol(print_mode, :leq), " ", set.upper)
 end
 
 function in_set_string(print_mode, set::MOI.GreaterThan)
-    return string(math_symbol(print_mode, :geq), " ", set.lower)
+    return string(_math_symbol(print_mode, :geq), " ", set.lower)
 end
 
 function in_set_string(print_mode, set::MOI.EqualTo)
-    return string(math_symbol(print_mode, :eq), " ", set.value)
+    return string(_math_symbol(print_mode, :eq), " ", set.value)
 end
 
 function in_set_string(print_mode, set::MOI.Interval)
-    return string(math_symbol(print_mode, :in), " ",
-                  math_symbol(print_mode, :open_rng), set.lower, ", ",
-                  set.upper, math_symbol(print_mode, :close_rng))
+    return string(_math_symbol(print_mode, :in), " ",
+                  _math_symbol(print_mode, :open_rng), set.lower, ", ",
+                  set.upper, _math_symbol(print_mode, :close_rng))
 end
 
 in_set_string(print_mode, ::MOI.ZeroOne) = "binary"
@@ -516,7 +516,7 @@ Return a `String` representing the membership to the set `set` using print mode
 `print_mode`.
 """
 function in_set_string(print_mode, set::MOI.AbstractSet)
-    return string(math_symbol(print_mode, :in), " ", set)
+    return string(_math_symbol(print_mode, :in), " ", set)
 end
 
 """
@@ -539,7 +539,7 @@ function constraint_string(print_mode, constraint_name,
                            constraint_object::AbstractConstraint)
     constraint_without_name = constraint_string(print_mode, constraint_object)
     if print_mode == IJuliaMode
-        constraint_without_name = wrap_in_inline_math_mode(constraint_without_name)
+        constraint_without_name = _wrap_in_inline_math_mode(constraint_without_name)
     end
     if isempty(constraint_name)
         return constraint_without_name
@@ -572,8 +572,8 @@ end
 
 function Base.show(io::IO, ::MIME"text/latex", c::NonlinearConstraintRef)
     constraint = c.model.nlp_data.nlconstr[c.index.value]
-    print(io, wrap_in_math_mode(nl_constraint_string(c.model, IJuliaMode,
-                                                    constraint)))
+    print(io, _wrap_in_math_mode(nl_constraint_string(c.model, IJuliaMode,
+                                                      constraint)))
 end
 
 # TODO: Printing is inconsistent between regular constraints and nonlinear
@@ -582,17 +582,17 @@ function nl_constraint_string(model::Model, mode, c::_NonlinearConstraint)
     s = _sense(c)
     nl = nl_expr_string(model, mode, c.terms)
     if s == :range
-        out_str = "$(string_round(c.lb)) " * math_symbol(mode, :leq) * " $nl " *
-                  math_symbol(mode, :leq) * " " * string_round(c.ub)
+        out_str = "$(_string_round(c.lb)) " * _math_symbol(mode, :leq) *
+                  " $nl " * _math_symbol(mode, :leq) * " " * _string_round(c.ub)
     else
         if s == :<=
-            rel = math_symbol(mode, :leq)
+            rel = _math_symbol(mode, :leq)
         elseif s == :>=
-            rel = math_symbol(mode, :geq)
+            rel = _math_symbol(mode, :geq)
         else
-            rel = math_symbol(mode, :eq)
+            rel = _math_symbol(mode, :eq)
         end
-        out_str = string(nl," ",rel," ",string_round(_rhs(c)))
+        out_str = string(nl, " ", rel, " ", _string_round(_rhs(c)))
     end
     return out_str
 end
