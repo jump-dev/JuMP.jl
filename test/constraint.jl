759caf53360697c1448857efbcc24cfe6f4e9bf5
Edvin Åblad <edvin@ablad.nu>
2019-03-20 14:26:16 +0100

    Add tests for range of feasibility

--- test/constraint.jl
+++ test/constraint.jl
@@ -561,3 +561,100 @@ end
     # Expected shadow prices
     Dict("xeq" => -1.0))
 end
+
+function test_perturbation_range_of_feasibility(model_string, primal_solution, basis_status, feasibility_ranges)
+    model = JuMP.Model()
+    MOIU.loadfromstring!(JuMP.backend(model), model_string)
+    JuMP.optimize!(model, with_optimizer(MOIU.MockOptimizer,
+                                         JuMP._MOIModel{Float64}(),
+                                         eval_variable_constraint_dual=false))
+    mock_optimizer = JuMP.backend(model).optimizer.model
+    MOI.set(mock_optimizer, MOI.TerminationStatus(), MOI.OPTIMAL)
+
+    JuMP.optimize!(model)
+    for (variable_name, value) in primal_solution
+        vi = MOI.get(JuMP.backend(model), MOI.VariableIndex, variable_name)
+        variable_ref = JuMP.VariableRef(model, vi)
+        MOI.set(mock_optimizer,  MOI.VariablePrimal(),
+         JuMP.optimizer_index(variable_ref), value)
+    end
+
+    for (constraint_name, value) in basis_status
+        ci = MOI.get(JuMP.backend(model), MOI.ConstraintIndex, constraint_name)
+        constraint_ref = JuMP.ConstraintRef(model, ci, JuMP.ScalarShape())
+        MOI.set(mock_optimizer,  MOI.ConstraintBasisStatus(),
+                JuMP.optimizer_index(constraint_ref), value)
+    end
+
+    @testset "feasibility range of $constraint_name" for constraint_name in keys(feasibility_ranges)
+        ci = MOI.get(JuMP.backend(model), MOI.ConstraintIndex, constraint_name)
+        constraint_ref = JuMP.ConstraintRef(model, ci, JuMP.ScalarShape())
+
+        true_range = feasibility_ranges[constraint_name]
+        range = perturbation_range_of_feasibility(constraint_ref)
+
+        @test true_range[1] ≈ range[1]
+        @test true_range[2] ≈ range[2]
+    end
+end
+
+
+@testset "range_of_feasibility" begin
+    test_perturbation_range_of_feasibility("""
+    variables: x, y, z, w
+    xint: x in Interval(-1.0, 1.0)
+    ylb: y >= 0.0
+    zval: z == 1.0
+    c1: x + y + z + w == 1.0
+    c2: x + y <= 2.0
+    """,
+    # Optimal primals
+    Dict("x" => 1.0, "y" => 1.0, "z" => 1.0, "w" => -3.0),
+    # Basis status
+    Dict("xint" => MOI.NONBASIC_AT_UPPER, "ylb" => MOI.BASIC, "zval" => MOI.NONBASIC, "c1" => MOI.NONBASIC, "c2" => MOI.NONBASIC),
+    # Expected perturbation ranges
+    Dict("ylb" => (-Inf, 1.0), "zval" => (-Inf, Inf), "c1" => (-Inf, Inf), "c2" => (-1.0, Inf)))
+
+    test_perturbation_range_of_feasibility("""
+    variables: x, y
+    xub: x <= 1.0
+    ylb: y >= 0.0
+    c1: x + y <= 2.0
+    c2: 2.0*x + y <= 3.5
+    """,
+    # Optimal primals
+    Dict("x" => 1.0, "y" => 1.0),
+    # Basis status
+    Dict("xub" => MOI.NONBASIC, "ylb" => MOI.BASIC, "c1" => MOI.NONBASIC, "c2" => MOI.BASIC),
+    # Expected perturbation ranges
+    Dict("xub" => (-Inf, 0.5), "ylb" => (-Inf, 1.0), "c1" => (-1.0, 0.5), "c2" => (-0.5, Inf)))
+
+    test_perturbation_range_of_feasibility("""
+    variables: x, y
+    xlb: x >= -1.0
+    xub: x <= 1.0
+    ylb: y >= 0.0
+    c1: x + y <= 2.0
+    c2: 2.0*x + y <= 3.0
+    """,
+    # Optimal primals
+    Dict("x" => 1.0, "y" => 1.0),
+    # Basis status
+    Dict("xlb" => MOI.BASIC, "xub" => MOI.NONBASIC, "ylb" => MOI.BASIC, "c1" => MOI.BASIC, "c2" => MOI.NONBASIC),
+    # Expected perturbation ranges
+    Dict("xlb" => (-Inf, 2.0), "xub" => (0.0, 0.5), "ylb" => (-Inf, 1.0), "c1" => (0.0, Inf), "c2" => (-1.0, 0.0)))
+
+    test_perturbation_range_of_feasibility("""
+    variables: x, y
+    xlb: x >= 0.0
+    ylb: y >= 0.0
+    c1: x + y >= 2.0
+    c2: x + -1.0*y in Interval(-1.0, 1.0)
+    """,
+    # Optimal primals
+    Dict("x" => 1.5, "y" => 0.5),
+    # Basis status
+    Dict("xlb" => MOI.BASIC, "ylb" => MOI.BASIC, "c1" => MOI.NONBASIC, "c2" => MOI.NONBASIC_AT_UPPER),
+    # Expected perturbation ranges
+    Dict("xlb" => (-Inf, 1.5), "ylb" => (-Inf, 0.5), "c1" => (-1.0, Inf)))
+end
