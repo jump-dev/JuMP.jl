cf5657a6598cb8ce229cc1ae6432c34dc41bf68e
Gauthier Soleilhac <gsoleilhac@gmail.com>
2019-06-22 18:26:30 +0200

    Add optional kwargs to optimize! for optimize_hook (#1987)
    * Add optional kwargs to optimize! for optimize_hook
    
    * Add test for unexpected kwarg error
    
    * Add documentation on kwargs to optimize!
    
    * Formatting tweak

--- src/optimizer_interface.jl
+++ src/optimizer_interface.jl
@@ -70,13 +70,18 @@ end
 """
     optimize!(model::Model,
               optimizer_factory::Union{Nothing, OptimizerFactory}=nothing;
-              ignore_optimize_hook=(model.optimize_hook === nothing))
+              bridge_constraints::Bool=true,
+              ignore_optimize_hook=(model.optimize_hook === nothing),
+              kwargs...)
 
 Optimize the model. If `optimizer_factory` is not `nothing`, it first sets the
 optimizer to a new one created using the optimizer factory. The factory can be
 created using the [`with_optimizer`](@ref) function. If `optimizer_factory` is
 `nothing` and no optimizer was set to `model` before calling this function, a
 [`NoOptimizer`](@ref) error is thrown.
+
+Keyword arguments `kwargs` are passed to the `optimize_hook`. An error is
+thrown if `optimize_hook` is `nothing` and keyword arguments are provided.
 
 ## Examples
 
@@ -99,7 +104,8 @@ optimize!(model, with_optimizer(GLPK.Optimizer))
 function optimize!(model::Model,
                    optimizer_factory::Union{Nothing, OptimizerFactory}=nothing;
                    bridge_constraints::Bool=true,
-                   ignore_optimize_hook=(model.optimize_hook === nothing))
+                   ignore_optimize_hook=(model.optimize_hook === nothing),
+                   kwargs...)
     # The nlp_data is not kept in sync, so re-set it here.
     # TODO: Consider how to handle incremental solves.
     if model.nlp_data !== nothing
@@ -122,8 +128,10 @@ function optimize!(model::Model,
     # If the user or an extension has provided an optimize hook, call
     # that instead of solving the model ourselves
     if !ignore_optimize_hook
-        return model.optimize_hook(model)
+        return model.optimize_hook(model; kwargs...)
     end
+
+    isempty(kwargs) || error("Unrecognized keyword arguments: $(join([k[1] for k in kwargs], ", "))")
 
     if mode(model) != DIRECT && MOIU.state(backend(model)) == MOIU.NO_OPTIMIZER
         throw(NoOptimizer())
