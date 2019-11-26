3e27c6ea9ecd09d928afd40ae1640d773767f134
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2019-11-26 18:38:43 +0100

    Update to MOF#master.

--- src/file_formats.jl
+++ src/file_formats.jl
@@ -1,31 +1,28 @@
 function write_to_file(model::Model, io::IO, format::MathOptFormat.FileFormat; kwargs...)
     @assert format != AUTOMATIC_FILE_FORMAT
-    dest = MathOptFormat._file_formats[format]()
+    dest = MathOptFormat._file_formats[format][2]()
     MOI.copy_to(dest, backend(model))
     MOI.write_to_file(dest, io)
     return
 end
 
 """
-    write_to_file(model::Model, filename::String; format::FileFormat=AUTOMATIC_FILE_FORMAT, compression::FileCompression=AUTOMATIC_FILE_COMPRESSION, kwargs...)
+    write_to_file(model::Model, filename::String; format::FileFormat=AUTOMATIC_FILE_FORMAT, compression::MathOptFormat.AbstractCompressionScheme=MathOptFormat.AutomaticCompression(), kwargs...)
 
 Write `model` to the file called `filename` using the format `format`.
 
 Valid formats are given by the enum [`FileFormat`](@ref). Valid compression algorithms
-are given by the enum [`FileCompression`](@ref).
+are given by a concrete subtype of [`AbstractCompressionScheme`](@ref).
 
 For keyword options, see [MathOptFormat.jl](https://github.com/odow/MathOptFormat.jl).
 """
 function write_to_file(model::Model, filename::String;
                        format::MathOptFormat.FileFormat=MathOptFormat.AUTOMATIC_FILE_FORMAT,
-                       compression::MathOptFormat.FileCompression=AUTOMATIC_FILE_COMPRESSION,
+                       compression::MathOptFormat.AbstractCompressionScheme=MathOptFormat.AutomaticCompression(),
                        kwargs...)
     if format == AUTOMATIC_FILE_FORMAT
-        format = _filename_to_format(filename)
+        format = MathOptFormat._filename_to_format(filename)
     end
-
-    MathOptFormat.gzip_open(filename, "w", compression=compression) do io
-        write_to_file(model, io, format; kwargs...)
-    end
+    MOI.write_to_file(model, filename, compression=compression)
     return
 end
