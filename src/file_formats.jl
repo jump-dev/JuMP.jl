2b8f9924d2966b2e0442f4dea576859b646bf48f
odow <o.dowson@gmail.com>
2019-12-03 19:26:00 -0600

    Add read/write to io streams

--- src/file_formats.jl
+++ src/file_formats.jl
@@ -22,6 +22,8 @@ Supported formats include:
  - `format = MOF.FORMAT_MPS`. Inferred if filename ends in `.mps`.
  - `format = MOF.FORMAT_LP`. Inferred if filename ends in `.lp`.
 
+ See `MathOptFormat.FileFormat` for all supported formats.
+
 Supported compression formats include:
  - `compression = MOF.NoCompression()`
  - `compression = MOF.Gzip()`. Inferred if filename ends in `.gz`.
@@ -46,6 +48,30 @@ function write_to_file(
 end
 
 """
+    write_to_file(model::Model, io::IO; format::MOF.FileFormat)
+
+Write the JuMP model `model` to `io` in the format `format`.
+
+Supported formats include:
+ - `format = MOF.FORMAT_CBF`. Inferred if filename ends in `.cbf`.
+ - `format = MOF.FORMAT_MOF`. Inferred if filename ends in `.mof.json`.
+ - `format = MOF.FORMAT_MPS`. Inferred if filename ends in `.mps`.
+ - `format = MOF.FORMAT_LP`. Inferred if filename ends in `.lp`.
+
+See `MathOptFormat.FileFormat` for all supported formats.
+"""
+function write_to_file(model::Model, io::IO; format::MOF.FileFormat)
+    if format == MOF.AUTOMATIC_FILE_FORMAT
+        error("Unable to infer the file format from an IO stream.")
+    end
+    dest = MOF._FILE_FORMATS[format][2]()
+    bridged_dest = MOI.Bridges.full_bridge_optimizer(dest, Float64)
+    MOI.copy_to(bridged_dest, backend(model))
+    MOI.write_to_file(dest, io)
+    return
+end
+
+"""
     read_from_file(
         filename::String;
         compression::MOF.AbstractCompressionScheme = MOF.AutomaticCompression(),
@@ -60,6 +86,8 @@ Supported formats include:
  - `format = MOF.FORMAT_MOF`. Inferred if filename ends in `.mof.json`.
  - `format = MOF.FORMAT_MPS`. Inferred if filename ends in `.mps`.
  - `format = MOF.FORMAT_LP`. Inferred if filename ends in `.lp`.
+
+See `MathOptFormat.FileFormat` for all supported formats.
 
 Supported compression formats include:
  - `compression = MOF.NoCompression()`
@@ -76,6 +104,30 @@ function read_from_file(
     src = MathOptFormat.read_from_file(
         filename; compression = compression, file_format = format
     )
+    model = Model()
+    MOI.copy_to(backend(model), src)
+    return model
+end
+
+"""
+    read_from_file(io::IO; format::MOF.FileFormat)
+
+Return a JuMP model read from `io` in the format `format`.
+
+Supported formats include:
+ - `format = MOF.FORMAT_CBF`. Inferred if filename ends in `.cbf`.
+ - `format = MOF.FORMAT_MOF`. Inferred if filename ends in `.mof.json`.
+ - `format = MOF.FORMAT_MPS`. Inferred if filename ends in `.mps`.
+ - `format = MOF.FORMAT_LP`. Inferred if filename ends in `.lp`.
+
+ See `MathOptFormat.FileFormat` for all supported formats.
+"""
+function read_from_file(io::IO; format::MOF.FileFormat)
+    if format == MOF.AUTOMATIC_FILE_FORMAT
+        error("Unable to infer the file format from an IO stream.")
+    end
+    src = MOF._FILE_FORMATS[format][2]()
+    MOI.read_from_file(src, io)
     model = Model()
     MOI.copy_to(backend(model), src)
     return model
--- test/file_formats.jl
+++ test/file_formats.jl
@@ -4,7 +4,7 @@
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 @testset "File formats" begin
-    @testset "MPS" begin
+    @testset "MOF" begin
         model = Model()
         @variable(model, x)
         @constraint(model, my_c, 3 * x >= 1)
@@ -47,8 +47,30 @@
         @test read("my_model.cbf", String) ==
             "VER\n3\n\nOBJSENSE\nMIN\n\nVAR\n3 1\nF 3\n\nOBJACOORD\n3\n0 1.0\n1 2.0\n2 1.0\n\nCON\n1 1\nL+ 1\n\nACOORD\n3\n0 0 1.0\n0 1 2.0\n0 2 1.0\n\nBCOORD\n1\n0 -1.0\n\nPSDCON\n1\n2\n\nHCOORD\n3\n0 0 0 0 1.0\n0 1 1 0 1.0\n0 2 1 1 1.0\n\n"
         model_2 = read_from_file("my_model.cbf")
-        @test sprint(print, model_2) ==
+        # Note: we replace ' in ' => ' ∈ ' because the unicode doesn't print on
+        # Windows systems for some reason.
+        @test replace(sprint(print, model_2), " in " => " ∈ ") ==
             "Min noname + 2 noname + noname\nSubject to\n [noname + 2 noname + noname - 1] ∈ MathOptInterface.Nonnegatives(1)\n [noname, noname, noname] ∈ MathOptInterface.PositiveSemidefiniteConeTriangle(2)\n"
         rm("my_model.cbf")
+    end
+    @testset "MOF io" begin
+        model = Model()
+        @variable(model, x)
+        @constraint(model, my_c, 3 * x >= 1)
+        @objective(model, Min, 2 * x^2 + x + 1)
+        io = IOBuffer()
+        @test_throws(
+            ErrorException("Unable to infer the file format from an IO stream."),
+            write_to_file(model, io; format = MOF.AUTOMATIC_FILE_FORMAT)
+        )
+        write_to_file(model, io; format = MOF.FORMAT_MOF)
+        seekstart(io)
+        @test_throws(
+            ErrorException("Unable to infer the file format from an IO stream."),
+            read_from_file(io; format = MOF.AUTOMATIC_FILE_FORMAT)
+        )
+        seekstart(io)
+        model_2 = read_from_file(io; format = MOF.FORMAT_MOF)
+        @test sprint(print, model) == sprint(print, model_2)
     end
 end
