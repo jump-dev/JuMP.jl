30d6589973ff55560b12a47906eed82209c2eed2
Henrique Becker <henriquebecker91@gmail.com>
2020-01-11 23:47:12 -0300

    Enhancing documentation by giving a one-liner for the user to check if this is the case.

--- src/constraints.jl
+++ src/constraints.jl
@@ -177,8 +177,9 @@ end
 
 Delete the constraints associated with `con_refs` from the model `model`.
 Solvers may implement specialized methods for deleting multiple constraints of
-the same concrete type. These may be more efficient than repeatedly calling the
-single constraint delete method.
+the same concrete type, i.e., when `isconcretetype(eltype(con_refs))`. These
+may be more efficient than repeatedly calling the single constraint delete
+method.
 """
 function delete(model::Model, con_refs::Vector{<:ConstraintRef{Model}})
     if any(c -> model !== c.model, con_refs)
