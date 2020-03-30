202994e7b538cc26f17d270069d41304242613a7
Akshay Sharma <akshay.sharma@procol.in>
2020-03-31 01:58:37 +0530

    edit docstring example

--- src/macros.jl
+++ src/macros.jl
@@ -656,8 +656,9 @@ adds groups of constraints at once, in the same fashion as @constraint. The mode
 Adds multiple variables to model at once, in the same fashion as `@variable` macro. The model must be the first argument, and multiple variables can be added on multiple lines wrapped in a `begin ... end` block. For example:
 
     @variables(m, begin
-      x
-      y
+  	x
+        y[1:2] >= 0
+        z, Bin, (start = 0)
     end)
 """ :(@variables)
 
