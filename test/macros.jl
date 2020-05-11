155c577f55658d424067d02a83861547a3a78469
Thibaut Cuvelier <dourouc05@gmail.com>
2020-05-11 17:26:05 +0200

    Use `@test_macro_throws` as suggested by @blegat

--- test/macros.jl
+++ test/macros.jl
@@ -188,7 +188,7 @@ function custom_function_test(ModelType::Type{<:JuMP.AbstractModel})
         @test jump_function(con) == x
         @test moi_set(con) isa CustomSet
 
-        @test_throws LoadError eval(:(@constraint(model, g(x))))
+        @test_macro_throws LoadError @constraint(model, g(x))
     end
 end
 
