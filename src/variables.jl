2bd4c36c0f51da66fcd2fe0fe718858e96f6c11b
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2020-08-20 18:27:48 +0200

    Typo in doc for value(v::VariableRef; result = 1)

--- src/variables.jl
+++ src/variables.jl
@@ -759,7 +759,7 @@ end
 Return the value of variable `v` associated with result index `result` of the
 most-recent returned by the solver.
 
-Use[`has_values`](@ref) to check if a result exists before asking for values.
+Use [`has_values`](@ref) to check if a result exists before asking for values.
 
 See also: [`result_count`](@ref).
 """
