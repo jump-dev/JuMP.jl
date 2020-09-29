7ff4cdac862fce17a48065c3fdcc2b37b6e86504
Rémi <47191601+remi-garcia@users.noreply.github.com>
2020-09-29 22:20:07 +0200

    Deal with fixed binary variables

--- src/variables.jl
+++ src/variables.jl
@@ -1097,6 +1097,10 @@ function relax_integrality(model::Model)
             if !info.has_fix
                 set_lower_bound(v, max(0.0, info.lower_bound))
                 set_upper_bound(v, min(1.0, info.upper_bound))
+            else
+                unfix(v)
+                set_lower_bound(v, max(0.0, info.fixed_value))
+                set_upper_bound(v, min(1.0, info.fixed_value))
             end
         end
     end
@@ -1117,6 +1121,10 @@ function relax_integrality(model::Model)
                     else
                         delete_upper_bound(v)
                     end
+                else
+                    delete_lower_bound(v)
+                    delete_upper_bound(v)
+                    fix(v, info.fixed_value)
                 end
             end
         end
