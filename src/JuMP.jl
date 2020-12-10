2fe13e61eca7197c42cef658be5ee92a535c8836
Oscar Dowson <odow@users.noreply.github.com>
2020-12-11 10:56:07 +1300

    Update JuMP.jl

--- src/JuMP.jl
+++ src/JuMP.jl
@@ -398,10 +398,27 @@ function add_bridge(model::Model,
 end
 
 """
-     print_bridge_graph(model::Model)
+     print_bridge_graph([io::IO,] model::Model)
 
-Print out the bridge graph explaining which bridges are used (and in what order
-thay are used) to bridge relevant constraints.
+Print the hyper-graph containing all variable, constraint, and objective types 
+that could be obtained by bridging the variables, constraints, and objectives 
+that are present in the model.
+
+Each node in the hyper-graph corresponds to a variable, constraint, or objective 
+type.
+  * Variable nodes are indicated by `[ ]`
+  * Constraint nodes are indicated by `( )`
+  * Objective nodes are indicated by `| |`
+The number inside each pair of brackets is an index of the node in the 
+hyper-graph.
+
+Note that this hyper-graph is the full list of possible transformations. When 
+the bridged model is created, we select the shortest hyper-path(s) from this 
+graph, so many nodes may be un-used.
+
+For more information, see Legat, B., Dowson, O., Garcia, J., and Lubin, M. 
+(2020).  "MathOptInterface: a data structure for mathematical optimization 
+problems." URL: https://arxiv.org/abs/2002.03447
 """
 print_bridge_graph(model::Model) = print_bridge_graph(Base.stdout, model)
 
