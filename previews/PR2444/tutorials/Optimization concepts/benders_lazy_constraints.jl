c18aabfd42830a5ba12f008244d51d60b5f6e7fd
Documenter.jl <documenter@juliadocs.github.io>
2021-02-02 03:06:40 +0000

    build based on 379aa60

--- previews/PR2444/tutorials/Optimization concepts/benders_lazy_constraints.jl
+++ previews/PR2444/tutorials/Optimization concepts/benders_lazy_constraints.jl
@@ -1,3 +1,23 @@
+# Copyright (c) 2019 Arpit Bhatia and JuMP contributors                          #src
+#                                                                                #src
+# Permission is hereby granted, free of charge, to any person obtaining a copy   #src
+# of this software and associated documentation files (the "Software"), to deal  #src
+# in the Software without restriction, including without limitation the rights   #src
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      #src
+# copies of the Software, and to permit persons to whom the Software is          #src
+# furnished to do so, subject to the following conditions:                       #src
+#                                                                                #src
+# The above copyright notice and this permission notice shall be included in all #src
+# copies or substantial portions of the Software.                                #src
+#                                                                                #src
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     #src
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       #src
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    #src
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         #src
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  #src
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #src
+# SOFTWARE.                                                                      #src
+
 # # [Benders Decomposition (Lazy Constraints)](@id benders_decomposition_lazy)
 
 # **Originally Contributed by**: Mathieu Besançon
