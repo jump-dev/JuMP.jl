0ab4a4c8510cc2314d1ee121458fce4189accced
Oscar Dowson <odow@users.noreply.github.com>
2021-02-16 21:14:45 +1300

    Add missing docstrings in sd.jl (#2473)

--- src/sd.jl
+++ src/sd.jl
@@ -1,3 +1,18 @@
+"""
+    SymMatrixSpace()
+
+Use in the [`@variable`](@ref) macro to constrain a matrix of variables to be
+symmetric.
+
+## Examples
+
+```jldoctest; setup=:(model = Model())
+julia> @variable(model, Q[1:2, 1:2] in SymMatrixSpace())
+2×2 LinearAlgebra.Symmetric{VariableRef,Array{VariableRef,2}}:
+ Q[1,1]  Q[1,2]
+ Q[1,2]  Q[2,2]
+```
+"""
 struct SymMatrixSpace end
 
 """
