e89dbdcd210f0871111bd31bd8cbd2b3fb1bb637
Oscar Dowson <odow@users.noreply.github.com>
2021-03-12 08:25:24 +1300

    Improve performance of value(NonlinearExpression) (#2515)

--- src/_Derivatives/forward.jl
+++ src/_Derivatives/forward.jl
@@ -36,7 +36,7 @@ function forward_eval(
     adj,
     const_values,
     parameter_values,
-    x_values::AbstractVector{T},
+    x_values,
     subexpression_values,
     user_input_buffer,
     user_output_buffer,
@@ -54,7 +54,7 @@ function forward_eval(
         # compute the value of node k
         @inbounds nod = nd[k]
         partials_storage[k] = zero(T)
-        if nod.nodetype == VARIABLE
+        if nod.nodetype == VARIABLE || nod.nodetype == MOIVARIABLE
             @inbounds storage[k] = x_values[nod.index]
         elseif nod.nodetype == VALUE
             @inbounds storage[k] = const_values[nod.index]
