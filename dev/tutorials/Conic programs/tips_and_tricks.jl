51ffc30ddabdea1a4ffa815315a10f6aa49ff776
Documenter.jl <documenter@juliadocs.github.io>
2021-03-23 04:39:23 +0000

    build based on 795fd25

--- dev/tutorials/Conic programs/tips_and_tricks.jl
+++ dev/tutorials/Conic programs/tips_and_tricks.jl
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #src
 # SOFTWARE.                                                                      #src
 
-# # Conic Programming
+# # Tips and Tricks
 
 # **Originally Contributed by**: Arpit Bhatia
 
@@ -30,18 +30,24 @@
 using JuMP
 import SCS
 import LinearAlgebra
-import Random
 
-Random.seed!(1234)
+import Random      # hide
+Random.seed!(1234) # hide
+nothing            # hide
 
-# ## What is a Cone?
+# !!! tip
+#     A good resource for learning more about functions which can be modelled
+#     using cones is the [MOSEK Modeling Cookbook](https://docs.mosek.com/modeling-cookbook/index.html).
+
+# ## What is a cone?
 
 # A subset $C$ of a vector space $V$ is a cone if $\forall x \in C$ and positive
-# scalars $\alpha$, the product $\alpha x \in C$. A cone C is a convex cone if
-# $\alpha x + \beta y \in C$, for any positive scalars $\alpha, \beta$, and any
-# $x, y \in C$.
+# scalars $\lambda > 0$, the product $\lambda x \in C$.
 
-# ## Conic Programming
+# A cone $C$ is a convex cone if $\lambda x + (1 - \lambda) y \in C$, for any
+# $\lambda \in [0, 1]$, and any $x, y \in C$.
+
+# ## What is a conic program?
 
 # Conic programming problems are convex optimization problems in which a convex
 # function is minimized over the intersection of an affine subspace and a convex
@@ -67,144 +73,46 @@ Random.seed!(1234)
 # where each $\mathcal{C}_i$ is a closed convex cone and $\mathcal{C}_i^*$ is
 # its dual cone.
 
-# ## Some of the Types of Cones Supported by JuMP
-
-# ### Second-Order Cone
+# ## Second-Order Cone
 
 # The Second-Order Cone (or Lorentz Cone) of dimension $n$ is of the form:
-#
 # ```math
-# Q^n = \{ (t,x) \in \mathbb{R}^\mbox{n} : t \ge ||x||_2 \}
+# Q^n = \{ (t, x) \in \mathbb{R}^n : t \ge ||x||_2 \}
 # ```
+
+# ### Example
+
+# Minimize the L2 norm of a vector $x$.
+
+model = Model()
+@variable(model, x[1:3])
+@variable(model, norm_x)
+@constraint(model, [norm_x; x] in SecondOrderCone())
+@objective(model, Min, norm_x)
+
+# ## Rotated Second-Order Cone
 
 # A Second-Order Cone rotated by $\pi/4$ in the $(x_1,x_2)$ plane is called a
 # Rotated Second-Order Cone. It is of the form:
-
 # ```math
-# Q_r^n = \{ (t,u,x) \in \mathbb{R}^\mbox{n} : 2tu \ge ||x||_2^2, t,u \ge 0 \}
+# Q_r^n = \{ (t,u,x) \in \mathbb{R}^n : 2tu \ge ||x||_2^2, t,u \ge 0 \}
 # ```
 
-# These cones are represented in JuMP using the MOI sets [`SecondOrderCone`](@ref)
-# and [`RotatedSecondOrderCone`](@ref0).
+# ### Example
 
-# #### Example: Euclidean Projection on a hyperplane
+# Given a set of predictors $x$, and observations $y$, find the parameter
+# $\theta$ that minimizes the sum of squares loss between $y_i$ and
+# $\theta x_i$.
 
-# For a given point $u_{0}$ and a set $K$, we refer to any point $u \in K$ which
-# is closest to $u_{0}$ as a projection of $u_{0}$ on $K$.
-#
-# The projection of a point $u_{0}$ on a hyperplane $K = \{u | p' \cdot u = q\}$
-# is given by:
+x = [1.0, 2.0, 3.0, 4.0]
+y = [0.45, 1.04, 1.51, 1.97]
+model = Model()
+@variable(model, θ)
+@variable(model, loss)
+@constraint(model, [loss; 0.5; θ .* x .- y] in RotatedSecondOrderCone())
+@objective(model, Min, loss)
 
-# ```math
-# \begin{aligned}
-# & \min & ||u - u_{0}|| \\
-# & \;\;\text{s.t.} & p' \cdot u = q  \\
-# \end{aligned}
-# ```
-
-u0 = rand(10)
-p = rand(10)
-q = rand();
-
-# We can model the above problem as the following conic program:
-
-# ```math
-# \begin{aligned}
-# & \min & t \\
-# & \;\;\text{s.t.} & p' \cdot u = q \\
-# & & (t, u - u_{0}) \in Q^{n+1}
-# \end{aligned}
-# ```
-
-# On comparing this with the primal form of a conic problem we saw above,
-
-# ```math
-# \begin{aligned}
-# & x = (t , u) &\\
-# & a_0 = e_1 &\\
-# & b_0 = 0 &\\
-# & A_1 = (0, p) &\\
-# & b_1 = -q &\\
-# & C_1 = \mathbb{R}_- &\\
-# & A_2 = 1 &\\
-# & b_2 = -(0, u_0) &\\
-# & C_2 = Q^{n+1} &
-# \end{aligned}
-# ```
-
-# Thus, we can obtain the dual problem as:
-
-# ```math
-# \begin{aligned}
-# & \max & y_1 + (0, u_0)^T y_2 \\
-# & \;\;\text{s.t.} & e_1 - (0,p)^T y_1 - y_2 = 0 \\
-# & & y_1 \in \mathbb{R}_- \\
-# & & y_2 \in Q^{n+1}
-# \end{aligned}
-# ```
-
-model = Model(SCS.Optimizer)
-set_silent(model)
-@variable(model, u[1:10])
-@variable(model, t)
-@objective(model, Min, t)
-@constraint(model, [t, (u - u0)...] in SecondOrderCone())
-@constraint(model, u' * p == q)
-optimize!(model)
-
-#-
-
-objective_value(model)
-
-#-
-
-value.(u)
-
-#-
-
-e1 = [1, zeros(10)...]
-dual_model = Model(SCS.Optimizer)
-set_silent(model)
-@variable(dual_model, y1 <= 0)
-@variable(dual_model, y2[1:11])
-@objective(dual_model, Max, q * y1 + vcat(0, u0)' * y2)
-@constraint(dual_model, e1 - [0, p...] .* y1 - y2 .== 0)
-@constraint(dual_model, y2 in SecondOrderCone())
-optimize!(dual_model)
-
-#-
-
-objective_value(dual_model)
-
-
-# We can also have an equivalent formulation using a Rotated Second-Order Cone:
-
-# ```math
-# \begin{aligned}
-# & \min & t \\
-# & \;\;\text{s.t.} & p' \cdot u = q \\
-# & & (t, 1/2, u - u_{0})\in Q_r^{n+2}
-# \end{aligned}
-# ```
-
-model = Model(SCS.Optimizer)
-set_silent(model)
-@variable(model, u[1:10])
-@variable(model, t)
-@objective(model, Min, t)
-@constraint(model, [t, 0.5, (u - u0)...] in RotatedSecondOrderCone())
-@constraint(model, u' * p == q)
-optimize!(model)
-
-#-
-
-value.(u)
-
-# The difference here is that the objective in the case of the Second-Order Cone
-# is $||u - u_{0}||_2$, while in the case of a Rotated Second-Order Cone is
-# $||u - u_{0}||_2^2$. However, the value of x is the same for both.
-
-# ### Exponential Cone
+# ## Exponential Cone
 
 # An Exponential Cone is a set of the form:
 
@@ -212,13 +120,15 @@ value.(u)
 # K_{exp} = \{ (x,y,z) \in \mathbb{R}^3 : y \exp (x/y) \le z, y > 0 \}
 # ```
 
-# It is represented in JuMP using the MOI set `MOI.ExponentialCone`.
+model = Model()
+@variable(model, x[1:3] >= 0)
+@constraint(model, x in MOI.ExponentialCone())
+@objective(model, Min, x[3])
 
-# #### Example: Entropy Maximization
+# ### Example: Entropy Maximization
 
-# As the name suggests, the entropy maximization problem consists of maximizing
-# the entropy function, $H(x) = -x\log{x}$ subject to linear inequality
-# constraints.
+# The entropy maximization problem consists of maximizing the entropy function,
+# $H(x) = -x\log{x}$ subject to linear inequality constraints.
 
 # ```math
 # \begin{aligned}
@@ -246,10 +156,10 @@ value.(u)
 # \end{aligned}
 # ```
 
-n = 15;
-m = 10;
-A = randn(m, n);
-b = rand(m, 1);
+n = 15
+m = 10
+A = randn(m, n)
+b = rand(m, 1)
 
 model = Model(SCS.Optimizer)
 set_silent(model)
@@ -257,11 +167,8 @@ set_silent(model)
 @variable(model, x[1:n])
 @objective(model, Max, sum(t))
 @constraint(model, sum(x) == 1)
-@constraint(model, A * x .<= b )
-# Cannot use the exponential cone directly in JuMP, hence we use MOI to specify
-# the set.
+@constraint(model, A * x .<= b)
 @constraint(model, con[i = 1:n], [t[i], x[i], 1] in MOI.ExponentialCone())
-
 optimize!(model)
 
 #-
@@ -292,7 +199,7 @@ objective_value(model)
 
 # ```math
 # \begin{aligned}
-# \lambda_{1} = \max t \\
+# \lambda_{1} = \min t \\
 # \text { s.t. } t I-A \succeq 0
 # \end{aligned}
 # ```
@@ -315,6 +222,3 @@ objective_value(model)
 
 # For other cones supported by JuMP, check out the
 # [MathOptInterface Manual](http://jump.dev/MathOptInterface.jl/stable).
-
-# A good resource for learning more about functions which can be modelled using
-# cones is the [MOSEK Modeling Cookbook](https://docs.mosek.com/modeling-cookbook/index.html).
