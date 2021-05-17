c932500b555979035f108bae0266b18cd7d87c27
odow <o.dowson@gmail.com>
2021-05-18 11:19:20 +1200

    Clarify that delete does not call unregister

--- src/variables.jl
+++ src/variables.jl
@@ -231,6 +231,15 @@ isequal_canonical(v::VariableRef, other::VariableRef) = isequal(v, other)
 
 Delete the variable associated with `variable_ref` from the model `model`.
 
+Note that `delete` does not unregister the name from the model, so adding a new
+variable of the same name will throw an error. Use [`unregister`](@ref) to
+unregister the name after deletion as follows:
+```julia
+@variable(model, x)
+delete(model, x)
+unregister(model, :x)
+```
+
 See also: [`unregister`](@ref)
 """
 function delete(model::Model, variable_ref::VariableRef)
