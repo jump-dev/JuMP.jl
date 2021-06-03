570e48fa7f179d31b15f20c4aa5230c30c5e982f
Oscar Dowson <odow@users.noreply.github.com>
2021-06-04 09:35:57 +1200

    Update the JuMP logo to match modern Julia (#2584)
    Co-authored-by: cormullion <cormullion@mac.com>

--- docs/make.jl
+++ docs/make.jl
@@ -76,6 +76,8 @@ makedocs(
         prettyurls = get(ENV, "CI", nothing) == "true",
         analytics = "UA-44252521-1",
         collapselevel = 1,
+        assets = ["assets/extra_styles.css"],
+        sidebar_sitename = false,
     ),
     # `strict = true` causes Documenter to throw an error if the Doctests fail.
     strict = true,
