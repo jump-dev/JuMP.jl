d784f97c7edf03789888bda11c8f80f16dc69805
odow <o.dowson@gmail.com>
2021-06-28 10:46:14 +1200

    Clarify the documentation on user-defined gradients

--- src/nlp.jl
+++ src/nlp.jl
@@ -1861,16 +1861,16 @@ end
 Register the user-defined function `f` that takes `dimension` arguments in
 `model` as the symbol `s`. In addition, provide a gradient function `∇f`.
 
-The functions `f`and ∇f must support all subtypes of `Real` as arguments. Do not
-assume that the inputs are `Float64`.
+The functions `f`and `∇f` must support all subtypes of `Real` as arguments. Do
+not assume that the inputs are `Float64`.
 
 ## Notes
 
  * If the function `f` is univariate (i.e., `dimension == 1`), `∇f` must return
    a number which represents the first-order derivative of the function `f`.
  * If the function `f` is multi-variate, `∇f` must have a signature matching
-   `∇f(g::Vector{T}, args::T...) where {T<:Real}`, where the first argument is a
-   vector `g` that is modified in-place with the gradient.
+   `∇f(g::AbstractVector{T}, args::T...) where {T<:Real}`, where the first
+   argument is a vector `g` that is modified in-place with the gradient.
  * If `autodiff = true` and `dimension == 1`, use automatic differentiation to
    compute the second-order derivative information. If `autodiff = false`, only
    first-order derivative information will be used.
@@ -1892,7 +1892,7 @@ register(model, :foo, 1, f, ∇f; autodiff = true)
 model = Model()
 @variable(model, x[1:2])
 g(x::T, y::T) where {T<:Real} = x * y
-function ∇g(g::Vector{T}, x::T, y::T) where {T<:Real}
+function ∇g(g::AbstractVector{T}, x::T, y::T) where {T<:Real}
     g[1] = y
     g[2] = x
     return
