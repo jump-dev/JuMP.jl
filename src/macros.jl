803e2930574833e12bd0b298fee2857191c8edec
joaquim <joaquimgarcia@psr-inc.com>
2021-08-08 20:47:59 -0300

    format and better error

--- src/macros.jl
+++ src/macros.jl
@@ -1497,8 +1497,9 @@ function build_variable(
 )
     if length(variables) != length(sets)
         return _error(
-            "Dimensions of the vector of scalar variables and the vector of " *
-            "scalar sets don't match.",
+            "Dimensions must match. Got a vector of scalar variables with" *
+            "$(length(variables)) elements and a vector of " *
+            "scalar sets with $(length(sets)).",
         )
     end
     return VariableConstrainedOnCreation.(variables, sets)
