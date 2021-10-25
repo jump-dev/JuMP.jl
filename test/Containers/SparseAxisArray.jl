26c9dcac0b43718d6a7e1121df283f48b547f5e8
Oscar Dowson <odow@users.noreply.github.com>
2021-10-25 15:53:33 +1300

    Update SparseAxisArray.jl

--- test/Containers/SparseAxisArray.jl
+++ test/Containers/SparseAxisArray.jl
@@ -172,7 +172,7 @@ $(SparseAxisArray{Float64,2,Tuple{Symbol,Char}}) with 2 entries"""
             "closer to a dictionary with `N`-dimensional keys. If you encounter " *
             "this error and you didn't call `size` explicitly, it is because " *
             "you called a method that is unsupported for `SparseAxisArray`s. " *
-            "Consult the JuMP documentation for a list of supported operations."
+            "Consult the JuMP documentation for a list of supported operations.",
         )
         x = Containers.@container([i = 1:3, j=i:3], i + j)
         @test_throws err size(x)
