0c01879f99352666213bdf7ec93279df4b38e767
Warren Ochibobo <warren@senga.co>
2021-12-27 15:36:34 +0300

    Small change in Model documentation

--- src/JuMP.jl
+++ src/JuMP.jl
@@ -241,7 +241,7 @@ end
 """
     Model()
 
-Return a new JuMP model without any optimizer; the model is stored the model in
+Return a new JuMP model without any optimizer; the model is stored in
 a cache.
 
 Use [`set_optimizer`](@ref) to set the optimizer before calling
