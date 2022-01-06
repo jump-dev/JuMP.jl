7395fd5ac7a8e310a47fb84cc302953965d80833
Oscar Dowson <odow@users.noreply.github.com>
2022-01-07 12:09:13 +1300

    Update constraints.jl

--- src/constraints.jl
+++ src/constraints.jl
@@ -132,7 +132,7 @@ end
 """
     set_start_value(con_ref::ConstraintRef, value)
 
-Set the primal start value (MOI attribute `ConstraintPrimalStart`) of the
+Set the primal start value ([`MOI.ConstraintPrimalStart`](@ref)) of the
 constraint `con_ref` to `value`. To remove a primal start value set it to
 `nothing`.
 
@@ -179,7 +179,7 @@ end
 """
     start_value(con_ref::ConstraintRef)
 
-Return the primal start value (MOI attribute `ConstraintPrimalStart`) of the
+Return the primal start value ([`MOI.ConstraintPrimalStart`](@ref)) of the
 constraint `con_ref`.
 
 Note: If no primal start value has been set, `start_value` will return
