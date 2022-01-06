81c4c642e6dea2ac23d5bdfb0d8f895a93d7856e
Oscar Dowson <odow@users.noreply.github.com>
2022-01-06 15:03:39 +1300

    Update src/constraints.jl
    Co-authored-by: Joaquim Dias Garcia <joaquimdgarcia@gmail.com>

--- src/constraints.jl
+++ src/constraints.jl
@@ -133,7 +133,7 @@ end
     set_start_value(con_ref::ConstraintRef, value)
 
 Set the primal start value (MOI attribute `ConstraintPrimalStart`) of the
-constraint `con_ref` to `value`. To remove a dual start value set it to
+constraint `con_ref` to `value`. To remove a primal start value set it to
 `nothing`.
 
 See also [`start_value`](@ref).
