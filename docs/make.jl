2f713d6a1a7a1088407335799f64d238291f5394
Oscar Dowson <odow@users.noreply.github.com>
2022-02-04 09:56:41 +1300

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -229,7 +229,7 @@ function _add_moi_pages()
     !!! warning
         This documentation in this section is a copy of the official
         MathOptInterface documentation available at
-        [https://jump.dev/MathOptInterface.jl/v0.10.7](https://jump.dev/MathOptInterface.jl/v0.10.7).
+        [https://jump.dev/MathOptInterface.jl/v0.10.8](https://jump.dev/MathOptInterface.jl/v0.10.8).
         It is included here to make it easier to link concepts between JuMP and
         MathOptInterface.
     """
