464e8bea7f2b6bbcf7853f2466ffd28aae08172d
Oscar Dowson <odow@users.noreply.github.com>
2022-02-07 16:27:19 +1300

    Update docs/src/tutorials/algorithms/tsp_lazy_constraints.jl

--- docs/src/tutorials/algorithms/tsp_lazy_constraints.jl
+++ docs/src/tutorials/algorithms/tsp_lazy_constraints.jl
@@ -117,7 +117,7 @@ import Plots
 # thus, the weight (distance) of each edge is defined as follows.
 
 function generate_distance_matrix(n; random_seed = 1)
-    Random.seed!(Random.MersenneTwister(),random_seed)
+    Random.seed!(Random.MersenneTwister(), random_seed)
     X = 100 * rand(n)
     Y = 100 * rand(n)
     d = [sqrt((X[i] - X[j])^2 + (Y[i] - Y[j])^2) for i in 1:n, j in 1:n]
