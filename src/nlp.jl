1e0b6ee82bfec5e4fe210b70caff47610607e81c
Oscar Dowson <odow@users.noreply.github.com>
2022-02-22 14:33:22 +1300

    Add nl_dual_start_value (#2879)

--- src/nlp.jl
+++ src/nlp.jl
@@ -218,6 +218,79 @@ function dual(c::NonlinearConstraintRef)
     return nldata.nlconstr_duals[c.index.value]
 end
 
+"""
+    nl_dual_start_value(model::Model)
+
+Return the current value of the MOI attribute [`MOI.NLPBlockDualStart`](@ref).
+"""
+function nl_dual_start_value(model::Model)
+    return MOI.get(model, MOI.NLPBlockDualStart())
+end
+
+"""
+    set_nl_dual_start_value(
+        model::Model,
+        start::Union{Nothing,Vector{Float64}},
+    )
+
+Set the value of the MOI attribute [`MOI.NLPBlockDualStart`](@ref).
+
+The start vector corresponds to the Lagrangian duals of the nonlinear
+constraints, in the order given by [`all_nl_constraints`](@ref). That is, you
+must pass a single start vector corresponding to all of the nonlinear
+constraints in a single function call; you cannot set the dual start value of
+nonlinear constraints one-by-one. The example below demonstrates how to use
+[`all_nl_constraints`](@ref) to create a mapping between the nonlinear
+constraint references and the start vector.
+
+Pass `nothing` to unset a previous start.
+
+## Examples
+
+```jldoctest
+julia> model = Model();
+
+julia> @variable(model, x[1:2]);
+
+julia> nl1 = @NLconstraint(model, x[1] <= sqrt(x[2]));
+
+julia> nl2 = @NLconstraint(model, x[1] >= exp(x[2]));
+
+julia> start = Dict(nl1 => -1.0, nl2 => 1.0);
+
+julia> start_vector = [start[con] for con in all_nl_constraints(model)]
+2-element Vector{Float64}:
+ -1.0
+  1.0
+
+julia> set_nl_dual_start_value(model, start_vector)
+
+julia> nl_dual_start_value(model)
+2-element Vector{Float64}:
+ -1.0
+  1.0
+```
+"""
+function set_nl_dual_start_value(model::Model, start::Vector{Float64})
+    _init_NLP(model)
+    nldata::_NLPData = model.nlp_data
+    if length(nldata.nlconstr) != length(start)
+        throw(
+            ArgumentError(
+                "length start vector ($(length(start))) does not match the " *
+                "number of nonlinear constraints ($(length(nldata.nlconstr))).",
+            ),
+        )
+    end
+    MOI.set(model, MOI.NLPBlockDualStart(), start)
+    return
+end
+
+function set_nl_dual_start_value(model::Model, start::Nothing)
+    MOI.set(model, MOI.NLPBlockDualStart(), start)
+    return
+end
+
 mutable struct _FunctionStorage
     nd::Vector{NodeData}
     adj::SparseMatrixCSC{Bool,Int}
