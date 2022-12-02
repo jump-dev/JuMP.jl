7bc0d7b222f00d59dfa18a80ccaf38ee614fec0d
odow <o.dowson@gmail.com>
2022-12-02 15:24:14 +1300

    Fix docstring

--- src/constraints.jl
+++ src/constraints.jl
@@ -1362,12 +1362,12 @@ constraints.
 
 ## Reformulation
 
-See [`Utilities.ScalarPenaltyRelaxation`](@ref) for details of the
+See [`MOI.Utilities.ScalarPenaltyRelaxation`](@ref) for details of the
 reformulation.
 
 For each constraint `ci`, the penalty passed to
-[`Utilities.ScalarPenaltyRelaxation`](@ref) is `get(penalties, ci, default)`. If
-the value is `nothing`, because `ci` does not exist in `penalties` and
+[`MoI.Utilities.ScalarPenaltyRelaxation`](@ref) is `get(penalties, ci, default)`.
+If the value is `nothing`, because `ci` does not exist in `penalties` and
 `default = nothing`, then the constraint is skipped.
 
 ## Return value
