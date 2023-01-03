d8a326237d9f0cb2f8474a8a8bd741762a1564a8
Oscar Dowson <odow@users.noreply.github.com>
2023-01-03 16:53:47 +1300

    Tidy test/mutable_arithmetics.jl (#3166)

--- test/mutable_arithmetics.jl
+++ test/mutable_arithmetics.jl
@@ -10,35 +10,64 @@
 
 module TestMutableArithmetics
 
-using LinearAlgebra
 using JuMP
 using Test
 
-const MA = JuMP._MA
-
 include(joinpath(@__DIR__, "JuMPExtension.jl"))
 
+function runtests()
+    for name in names(@__MODULE__; all = true)
+        if startswith("$(name)", "test_")
+            @testset "$(name)" begin
+                getfield(@__MODULE__, name)()
+            end
+        end
+        if startswith("$(name)", "test_extension_")
+            # Note(odow): We disable JuMPExtension tests for MutableArithmetics
+            # because they take far to looooooong. MutableArithmetics is a
+            # tested package. We're also testing that it works with JuMP. We
+            # don't need to double up on our tests.
+            if name != "test_promote_operation"
+                continue
+            end
+            @testset "$(name)-JuMPExtension" begin
+                getfield(@__MODULE__, name)(
+                    JuMPExtension.MyModel,
+                    JuMPExtension.MyVariableRef,
+                )
+            end
+        end
+    end
+    return
+end
+
 struct DummyVariableRef <: JuMP.AbstractVariableRef end
+
 JuMP.name(::DummyVariableRef) = "dummy"
+
 struct DummyExpr end
+
 function Base.:(+)(
     ::GenericAffExpr{Float64,DummyVariableRef},
     ::JuMP.AbstractJuMPScalar,
 )
     return DummyExpr()
 end
+
 function Base.:(+)(
     ::JuMP.AbstractJuMPScalar,
     ::JuMP.GenericAffExpr{Float64,DummyVariableRef},
 )
     return DummyExpr()
 end
+
 function Base.:(-)(
     ::GenericAffExpr{Float64,DummyVariableRef},
     ::JuMP.AbstractJuMPScalar,
 )
     return DummyExpr()
 end
+
 function Base.:(-)(
     ::JuMP.AbstractJuMPScalar,
     ::JuMP.GenericAffExpr{Float64,DummyVariableRef},
@@ -50,9 +79,13 @@ function promote_operation_test(op::Function, x::Type, y::Type)
     f() = JuMP._MA.promote_operation(op, x, y)
     @test typeof(op(zero(x), zero(y))) == f()
     @test 0 == @allocated f()
+    return
 end
 
-function test_promote_operation(ModelType, VariableRefType)
+function test_extension_promote_operation(
+    ModelType = Model,
+    VariableRefType = VariableRef,
+)
     for S in [Float64, ComplexF64]
         AffExprType = JuMP.GenericAffExpr{S,VariableRefType}
         QuadExprType = JuMP.GenericQuadExpr{S,VariableRefType}
@@ -76,17 +109,18 @@ function test_promote_operation(ModelType, VariableRefType)
             end
         end
     end
+    return
 end
 
-function test_int(ModelType, ::Any)
+function test_extension_int(ModelType = Model, VariableRefType = VariableRef)
     model = ModelType()
     @variable(model, x)
     for a in [1, 1im]
-        MA.Test.int_test(
+        JuMP._MA.Test.int_test(
             typeof(a * x);
             exclude = ["int_mul", "int_add", "int_add_mul"],
         )
-        MA.Test.int_test(
+        JuMP._MA.Test.int_test(
             typeof(a * x^2);
             exclude = ["int_mul", "int_add", "int_add_mul"],
         )
@@ -94,101 +128,91 @@ function test_int(ModelType, ::Any)
     return
 end
 
-function test_scalar(ModelType, ::Any)
+function test_extension_scalar(ModelType = Model, VariableRefType = VariableRef)
     model = ModelType()
     @variable(model, x)
     exclude = ["cube"]
-    MA.Test.scalar_test(x; exclude = exclude)
+    JuMP._MA.Test.scalar_test(x; exclude = exclude)
     for a in [2, 2im]
-        MA.Test.scalar_test(a * x + 3; exclude = exclude)
-        MA.Test.scalar_test(a * x^2 + 4x + 1; exclude = exclude)
+        JuMP._MA.Test.scalar_test(a * x + 3; exclude = exclude)
+        JuMP._MA.Test.scalar_test(a * x^2 + 4x + 1; exclude = exclude)
     end
     return
 end
 
-function test_quadratic(ModelType, ::Any)
+function test_extension_quadratic(
+    ModelType = Model,
+    VariableRefType = VariableRef,
+)
     model = ModelType()
     @variable(model, w)
     @variable(model, x)
     @variable(model, y)
     @variable(model, z)
-    return MA.Test.quadratic_test(w, x, y, z)
+    JuMP._MA.Test.quadratic_test(w, x, y, z)
+    return
 end
 
-function test_sparse(ModelType, ::Any)
+function test_extension_sparse(ModelType = Model, VariableRefType = VariableRef)
     model = ModelType()
     @variable(model, X11)
     @variable(model, X23)
     @variable(model, Xd[1:3, 1:3])
-    return MA.Test.sparse_test(X11, X23, Xd)
-end
-
-function test_vector(ModelType, ::Any)
-    model = ModelType()
-    @variable(model, x[1:3])
-    return MA.Test.array_test(x)
-end
-
-function test_symmetric_matrix(ModelType, ::Any)
-    model = ModelType()
-    @variable(model, y[1:2, 1:2], Symmetric)
-    return MA.Test.array_test(y)
-end
-
-function test_nonsquare_matrix(ModelType, ::Any)
-    model = ModelType()
-    @variable(model, z[1:2, 1:3])
-    return MA.Test.array_test(z)
-end
-
-function test_DenseAxisVector(ModelType, ::Any)
-    model = ModelType()
-    @variable(model, y[2:5])
-    return MA.Test.array_test(y; exclude = ["matrix_vector", "non_array"])
-end
-
-function test_different_variables(ModelType, ::Any)
-    model = ModelType()
-    x = @variable(model)
-    y = DummyVariableRef()
-    function _promote_test(a, b)
-        A = typeof(a)
-        B = typeof(b)
-        @test MA.promote_operation(+, A, B) == DummyExpr
-        @test MA.promote_operation(-, A, B) == DummyExpr
-        @test MA.promote_operation(+, B, A) == DummyExpr
-        @test MA.promote_operation(-, B, A) == DummyExpr
-    end
-    _promote_test(x, y)
-    for aff in [x + 1, x + im]
-        _promote_test(aff, y)
-    end
+    JuMP._MA.Test.sparse_test(X11, X23, Xd)
     return
 end
 
-function runtests()
-    for name in names(@__MODULE__; all = true)
-        if !startswith("$(name)", "test_")
-            continue
-        end
-        f = getfield(@__MODULE__, name)
-        @testset "$(name)" begin
-            f(Model, VariableRef)
-        end
-        # Note(odow): We disable JuMPExtension tests for MutableArithmetics
-        # because they take far to looooooong. MutableArithmetics is a
-        # tested package. We're also testing that it works with JuMP. We
-        # don't need to double up on our tests.
-        # @testset "$(name)-JuMPExtension" begin
-        #     f(JuMPExtension.MyModel, JuMPExtension.MyVariableRef)
-        # end
+function test_extension_vector(ModelType = Model, VariableRefType = VariableRef)
+    model = ModelType()
+    @variable(model, x[1:3])
+    JuMP._MA.Test.array_test(x)
+    return
+end
+
+function test_extension_symmetric_matrix(
+    ModelType = Model,
+    VariableRefType = VariableRef,
+)
+    model = ModelType()
+    @variable(model, y[1:2, 1:2], Symmetric)
+    JuMP._MA.Test.array_test(y)
+    return
+end
+
+function test_extension_nonsquare_matrix(
+    ModelType = Model,
+    VariableRefType = VariableRef,
+)
+    model = ModelType()
+    @variable(model, z[1:2, 1:3])
+    JuMP._MA.Test.array_test(z)
+    return
+end
+
+function test_extension_DenseAxisVector(
+    ModelType = Model,
+    VariableRefType = VariableRef,
+)
+    model = ModelType()
+    @variable(model, y[2:5])
+    JuMP._MA.Test.array_test(y; exclude = ["matrix_vector", "non_array"])
+    return
+end
+
+function test_extension_different_variables(
+    ModelType = Model,
+    VariableRefType = VariableRef,
+)
+    model = ModelType()
+    x = @variable(model)
+    y = DummyVariableRef()
+    for a in (x, x + 1, x + im)
+        @test JuMP._MA.promote_operation(+, typeof(a), typeof(y)) == DummyExpr
+        @test JuMP._MA.promote_operation(-, typeof(a), typeof(y)) == DummyExpr
+        @test JuMP._MA.promote_operation(+, typeof(y), typeof(a)) == DummyExpr
+        @test JuMP._MA.promote_operation(-, typeof(y), typeof(a)) == DummyExpr
     end
-    @testset "test_promote_operation-JuMPExtension" begin
-        test_promote_operation(
-            JuMPExtension.MyModel,
-            JuMPExtension.MyVariableRef,
-        )
-    end
+    return
 end
 
 end
