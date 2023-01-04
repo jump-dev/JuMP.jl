003b63ce11e3aae55fd1c48aa04916ac8fe37c8e
odow <o.dowson@gmail.com>
2023-01-04 17:32:11 +1300

    Tidy test/nonnegative_bridge.jl

--- test/nonnegative_bridge.jl
+++ test/nonnegative_bridge.jl
@@ -8,13 +8,6 @@
 # See https://github.com/jump-dev/JuMP.jl
 #############################################################################
 
-# This file contains an example bridge used for tests.
-
-using JuMP
-
-const MOIB = MOI.Bridges
-const MOIBC = MOI.Bridges.Constraint
-
 """
     Nonnegative <: MOI.AbstractScalarSet
 
@@ -31,17 +24,21 @@ The `NonnegativeBridge` replaces a constraint `func`-in-`Nonnegative` into
 `func`-in-`GreaterThan{T}`.
 """
 struct NonnegativeBridge{T,F<:MOI.AbstractScalarFunction} <:
-       MOIBC.AbstractBridge
+       MOI.Bridges.Constraint.AbstractBridge
     constraint_index::MOI.ConstraintIndex{F,MOI.GreaterThan{T}}
 end
 
-function MOIBC.bridge_constraint(
+function MOI.Bridges.Constraint.bridge_constraint(
     ::Type{NonnegativeBridge{T,F}},
-    model,
+    model::MOI.ModelLike,
     f::F,
     s::Nonnegative,
 ) where {T,F}
-    ci = MOIU.normalize_and_add_constraint(model, f, MOI.GreaterThan(zero(T)))
+    ci = MOI.Utilities.normalize_and_add_constraint(
+        model,
+        f,
+        MOI.GreaterThan(zero(T)),
+    )
     return NonnegativeBridge{T,F}(ci)
 end
 
@@ -53,31 +50,37 @@ function MOI.supports_constraint(
     return true
 end
 
-function MOIB.added_constrained_variable_types(::Type{<:NonnegativeBridge})
+function MOI.Bridges.added_constrained_variable_types(
+    ::Type{<:NonnegativeBridge},
+)
     return Tuple{Type}[]
 end
-function MOIB.added_constraint_types(::Type{NonnegativeBridge{T,F}}) where {T,F}
+
+function MOI.Bridges.added_constraint_types(
+    ::Type{NonnegativeBridge{T,F}},
+) where {T,F}
     return [(F, MOI.GreaterThan{T})]
 end
-function MOIBC.concrete_bridge_type(
+
+function MOI.Bridges.Constraint.concrete_bridge_type(
     ::Type{NonnegativeBridge{T}},
     F::Type{<:MOI.AbstractScalarFunction},
     ::Type{Nonnegative},
 ) where {T}
     # In the constructor, the function `f` of type `F` is passed to
-    # `MOIU.normalize_and_add_constraint` which removes the constrant from `f` but
-    # does not change its type so the type of the function in `MOI.GreaterThan`
-    # will also be `F`.
+    # `MOI.Utilities.normalize_and_add_constraint` which removes the constrant
+    # from `f` but does not change its type so the type of the function in
+    # `MOI.GreaterThan` will also be `F`.
     return NonnegativeBridge{T,F}
 end
 
-# Attributes, Bridge acting as an model
 function MOI.get(
     ::NonnegativeBridge{T,F},
     ::MOI.NumberOfConstraints{F,MOI.GreaterThan{T}},
-) where {T,F}
+)::Int64 where {T,F}
     return 1
 end
+
 function MOI.get(
     bridge::NonnegativeBridge{T,F},
     ::MOI.ListOfConstraintIndices{F,MOI.GreaterThan{T}},
@@ -85,12 +88,11 @@ function MOI.get(
     return [bridge.constraint_index]
 end
 
-# Indices
 function MOI.delete(model::MOI.ModelLike, bridge::NonnegativeBridge)
-    return MOI.delete(model, bridge.constraint_index)
+    MOI.delete(model, bridge.constraint_index)
+    return
 end
 
-# Attributes, Bridge acting as a constraint
 function MOI.get(
     model::MOI.ModelLike,
     attr::Union{MOI.ConstraintPrimal,MOI.ConstraintDual},
