a4521517d86767f8eec97d0a66ea7eb14d7cdbb2
odow <o.dowson@gmail.com>
2023-01-30 15:03:39 +1300

    Add syntax support for reified constraints

---
+++ test/test_reified.jl
@@ -0,0 +1,68 @@
+#  Copyright 2017, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+module TestReified
+
+using JuMP
+using Test
+
+include(joinpath(@__DIR__, "utilities.jl"))
+
+function test_reified_all_different()
+    model = Model()
+    @variable(model, 1 <= x[1:4] <= 4, Int)
+    @variable(model, z, Bin)
+    c = @constraint(model, z := {x in MOI.AllDifferent(4)})
+    in_sym = JuMP._math_symbol(MIME("text/plain"), :in)
+    set = MOI.AllDifferent(4)
+    @test sprint(show, c) == "z := {[x[1], x[2], x[3], x[4]] $in_sym $set}"
+    obj = constraint_object(c)
+    @test obj.func == [z; x]
+    @test obj.set == MOI.Reified(set)
+    return
+end
+
+function test_reified_equal_to()
+    model = Model()
+    @variable(model, x)
+    @variable(model, z, Bin)
+    c = @constraint(model, z := {x == 1})
+    @test sprint(show, c) == "z := {x = 1.0}"
+    obj = constraint_object(c)
+    @test obj.func == AffExpr[z, x]
+    @test obj.set == MOI.Reified(MOI.EqualTo(1.0))
+    return
+end
+
+function test_reified_inconsistent()
+    model = Model()
+    @variable(model, x)
+    @variable(model, z, Bin)
+    @test_macro_throws(
+        ErrorException(
+            "In `@constraint(model, \$(Expr(:(:=), :z, :({x .== 1}))))`: " *
+            "vectorized constraints cannot be used with reification.",
+        ),
+        @constraint(model, z := {x .== 1})
+    )
+    return
+end
+
+function test_reified_no_curly_bracket()
+    model = Model()
+    @variable(model, x)
+    @variable(model, z, Bin)
+    @test_macro_throws(
+        ErrorException(
+            "In `@constraint(model, \$(Expr(:(:=), :z, :(x == 1))))`: " *
+            "Invalid right-hand side `x == 1` of reified constraint. " *
+            "Expected constraint surrounded by `{` and `}`.",
+        ),
+        @constraint(model, z := x == 1)
+    )
+    return
+end
+
+end  # module
