56fca4072fd99ea36a039f4c85b8ca62110edaa6
James Foster <James.Foster@csiro.au>
2023-03-08 15:29:20 +1100

    Edit to highlight bilevel opt.

--- docs/src/tutorials/nonlinear/user_defined_hessians.jl
+++ docs/src/tutorials/nonlinear/user_defined_hessians.jl
@@ -18,64 +18,19 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #src
 # SOFTWARE.                                                                      #src
 
-# # User-defined Hessians
+# # User-defined Hessians for bilevel optimization
 
 # In this tutorial, we explain how to write a user-defined function (see
 # [User-defined Functions](@ref)) with a Hessian matrix explicitly provided by
-# the user.
+# the user. User-defined Hessian functions can be useful when solving more complicated
+# problems, such as bilevel optimization problems.
 
 # This tutorial uses the following packages:
 
 using JuMP
 import Ipopt
 
-# ## Rosenbrock example
-
-# As a simple example, we first consider the Rosenbrock function:
-
-rosenbrock(x...) = (1 - x[1])^2 + 100 * (x[2] - x[1]^2)^2
-
-# which has the gradient vector:
-
-function ∇rosenbrock(g::AbstractVector, x...)
-    g[1] = 400 * x[1]^3 - 400 * x[1] * x[2] + 2 * x[1] - 2
-    g[2] = 200 * (x[2] - x[1]^2)
-    return
-end
-
-# and the Hessian matrix:
-
-function ∇²rosenbrock(H::AbstractMatrix, x...)
-    H[1, 1] = 1200 * x[1]^2 - 400 * x[2] + 2
-    ## H[1, 2] = -400 * x[1] <-- not needed because Hessian is symmetric
-    H[2, 1] = -400 * x[1]
-    H[2, 2] = 200.0
-    return
-end
-
-# You may assume the Hessian matrix `H` is initialized with zeros, and
-# because it is symmetric you need only to fill in the non-zero of the
-# lower-triangular terms.
-
-# The matrix type passed in as `H` depends on the automatic differentiation
-# system, so make sure the first argument to the Hessian function supports an
-# `AbstractMatrix` (it may be something other than `Matrix{Float64}`). However,
-# you may assume only that `H` supports `size(H)` and `setindex!`.
-
-# Now that we have the function, its gradient, and its Hessian, we can construct
-# a JuMP model, register the function, and use it in a `@NL` macro:
-
-model = Model(Ipopt.Optimizer)
-@variable(model, x[1:2])
-register(model, :rosenbrock, 2, rosenbrock, ∇rosenbrock, ∇²rosenbrock)
-@NLobjective(model, Min, rosenbrock(x[1], x[2]))
-optimize!(model)
-solution_summary(model; verbose = true)
-
-# ## Bilevel optimization
-
-# User-defined Hessian functions can be useful when solving more complicated
-# problems. In the rest of this tutorial, our goal is to solve the bilevel
+# In the rest of this tutorial, our goal is to solve the bilevel
 # optimization problem:
 
 # ```math
