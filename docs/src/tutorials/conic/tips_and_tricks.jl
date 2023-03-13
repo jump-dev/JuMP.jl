e4749817b3c51996b329a26f052e6344334f822a
Oscar Dowson <odow@users.noreply.github.com>
2023-03-14 11:37:01 +1300

    [docs] add p-norm example (#3282)

--- docs/src/tutorials/conic/tips_and_tricks.jl
+++ docs/src/tutorials/conic/tips_and_tricks.jl
@@ -217,6 +217,28 @@ value(t), value(x)
 # that offers an alternative formulation that can be more efficient for some
 # formulations.
 
+# ## P-Norm
+
+# The p-norm ``||x||_p = \left(\sum\limits_{i} |x_i|^p\right)^{\frac{1}{p}}``
+# can be modeled using [`MOI.PowerCone`](@ref)s. See the [Mosek Modeling Cookbook](https://docs.mosek.com/modeling-cookbook/powo.html#p-norm-cones)
+# for the derivation.
+
+function p_norm(x::Vector, p)
+    N = length(x)
+    model = Model(SCS.Optimizer)
+    set_silent(model)
+    @variable(model, r[1:N])
+    @variable(model, t)
+    @constraint(model, [i = 1:N], [r[i], t, x[i]] in MOI.PowerCone(1 / p))
+    @constraint(model, sum(r) == t)
+    @objective(model, Min, t)
+    optimize!(model)
+    return value(t)
+end
+
+x = rand(5);
+LinearAlgebra.norm(x, 4), p_norm(x, 4)
+
 # ## Positive Semidefinite Cone
 
 # The set of positive semidefinite matrices (PSD) of dimension $n$ form a cone
