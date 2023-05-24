b8e7b04d3124aa48c5244a597abdfd3c641a9e4a
Oscar Dowson <odow@users.noreply.github.com>
2023-05-24 16:20:44 +1200

    Update test_dimensionaldata.jl

--- test/Containers/test_dimensionaldata.jl
+++ test/Containers/test_dimensionaldata.jl
@@ -1,13 +1,29 @@
+#  Copyright 2017, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 module TestContainersDimensionalData
 
-if isdefined(Base,:get_extension)
-    using JuMP, DimensionalData
-    @testset "DimensionalData.jl integration" begin 
-        model = JuMP.Model();
-        @test @variable(model, x[i=2:4, j=["a", "b"]], container = DimArray) isa DimArray
-        @test_throws ArgumentError @variable(model, z[i=1:3, j=i:2], container = DimArray)
-        @test_throws ArgumentError @variable(model, w[1:3, j=1:2], container = DimArray)
-    end   
-end
+using JuMP
+using DimensionalData
+    
+function test_dimension_data_integration()
+    if !isdefined(Base, :get_extension)
+        return
+    end
+    model = Model()
+    @variable(model, x[i = 2:4, j = ["a", "b"]], container = DimArray)
+    @test x isa DimArray
+    @test_throws(
+        ArgumentError,
+        @variable(model, z[i = 1:3, j = i:2], container = DimArray),
+    )
+    @test_throws(
+        ArgumentError,
+        @variable(model, w[1:3, j = 1:2], container = DimArray),
+    )
+    return
+end   
 
 end
