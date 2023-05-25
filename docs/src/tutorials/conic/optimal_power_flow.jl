f13f927a0aefde0af2d1528c6f86481f75ea80d5
James Foster <James.Foster@csiro.au>
2023-05-25 17:46:37 +1000

    Respond to changes, clarify wording

--- docs/src/tutorials/conic/optimal_power_flow.jl
+++ docs/src/tutorials/conic/optimal_power_flow.jl
@@ -7,15 +7,16 @@
 
 # *This tutorial was originally contributed by James Foster (@jd-foster).*
 
-# This tutorial formulates and solves an optimal power flow problem,
+# This tutorial formulates and solves an alternating current optimal power flow (AC-OPF) problem,
 # a much-studied nonlinear problem from the field of electrical engineering.
-# Our particular focus is on obtaining a good estimate of the objective value
+# Once we've formulated and solved the nonlinear problem, 
+# we will turn our focus to obtaining a good estimate of the objective value
 # at the global optimum through the use of semidefinite programming techniques.
 
-# The tutorial highlights JuMP's ability to formulate problems involving
+# The tutorial highlights JuMP's ability to directly formulate problems involving
 # complex-valued decision variables and complex matrix cones such as the 
 # [`HermitianPSDCone`](@ref) object.
-# See the [Complex number support](@ref) section of the manual for more details.
+# See also the [Complex number support](@ref) section of the manual for more details.
 
 # For another example of modeling with complex decision variables
 # see the [Quantum state discrimination](@ref) tutorial.
@@ -32,23 +33,29 @@ import SparseArrays
 import DataFrames
 import Test
 
-# ## Formulation
+# ## Initial formulation
 
 # Optimal power flow problems for electrical transmission typically pose the
 # following question: what is the most cost-effective operation of electricity
 # generators while meeting constraints on the safe limits of network components?
 
-# We will use the 9 _bus_ network test case `case9mod` to explore this problem
-# following the example of Krasko and Rebennack (2017).
-
-# Here *bus* and network *node* are taken as analogous terms, as are *branch* and transmission *line*.
-
-# The graph of the network shows three nodes each used for the different purposes of
+# The graph of the network shown here has nine nodes, with three each used for
+# the different purposes of
 # generation ``G`` (nodes 1, 2 and 3),
 # trans-shipment (nodes 4, 6 and 8) 
-# and demand ``L`` (5, 7, and 9).
+# and demand ``D`` (5, 7, and 9).
 
 # ![Nine Nodes](../../assets/case9mod.png)
+
+# We're using the 9-_bus_ network test case `case9mod` to explore this problem.
+# This test case is a version of the
+# [MATPOWER](https://matpower.org/) test case `case9`
+# ([archive](https://github.com/MATPOWER/matpower/tree/master/data))
+# that has been modified by Bukhsh et al. (2013) for their
+# test case archive of optimal power flow problems with local optima.
+# This test case is also extensively evaluated in Krasko and Rebennack (2017).
+
+# Here *bus* and network *node* are taken as analogous terms, as are *branch* and transmission *line*.
 
 # For future reference, let's name the number of nodes in the network:
 N = 9
@@ -57,14 +64,17 @@ N = 9
 # With the `sparsevec` function from the `SparseArrays` standard library package
 # we can just give the indices and values of the non-zero data points:
 
-# _Generation power lower (`lb`) and upper (`ub`) bounds (active and reactive)_
+# _Generation power bounds: lower (`lb`) and upper (`ub`)_
+
+# _Active_
 P_Gen_lb = SparseArrays.sparsevec([1, 2, 3], [10, 10, 10], N)
 P_Gen_ub = SparseArrays.sparsevec([1, 2, 3], [250, 300, 270], N)
 
+# _Reactive_
 Q_Gen_lb = SparseArrays.sparsevec([1, 2, 3], [-5, -5, -5], N)
 Q_Gen_ub = SparseArrays.sparsevec([1, 2, 3], [300, 300, 300], N)
 
-# _Power demand levels (active, reactive and complex form)_
+# _Power demand levels (real, reactive, and complex form)_
 P_Demand = SparseArrays.sparsevec([5, 7, 9], [54, 60, 75], N)
 Q_Demand = SparseArrays.sparsevec([5, 7, 9], [18, 21, 30], N)
 S_Demand = P_Demand + im * Q_Demand
@@ -73,8 +83,8 @@ S_Demand = P_Demand + im * Q_Demand
 # reactive power injections ``Q^G``over the allowed range of the generators.
 # All other buses must restrict their generation variables to 0.
 # On the other hand these non-generator nodes have a fixed 
-# real and reactive power demand, denoted ``P^L`` and ``Q^L`` respectively
-# (fixed at 0 in the case of trans-shipment and generator nodes).
+# real and reactive power demand, denoted ``P^D`` and ``Q^D`` respectively
+# (these are fixed at 0 in the case of trans-shipment and generator nodes).
 
 # The cost of operating each generator is here modeled as a quadratic function of its real power output;
 # in our specific test case, the objective function to minimize is
@@ -85,7 +95,7 @@ S_Demand = P_Demand + im * Q_Demand
 #          \;\; & 0.1225 \;  (P^G_3)^2 +     P^G_3 + 335 \\
 # \end{align}
 # ```
-# Let's create a basic JuMP model with these settings:
+# Let's create an initial JuMP model with some of this data:
 model = Model(Ipopt.Optimizer)
 set_silent(model)
 
@@ -105,15 +115,20 @@ set_silent(model)
     335
 )
 
-# Even before solving, we can substitute the lower bound on each generator's real power range 
+# Even before solving an optimization problem, we can estimate a lower bound on the
+# best objective value by substituting the lower bound on each generator's real power range 
 # (all 10, as it turns out in this case)
 
 objval_basic_lb = value(lower_bound, objective_function(model));
+Test.@test isapprox(objval_basic_lb, 1188.75, atol = 1e-2)  #src
 println("Objective value (basic lower bound): $(objval_basic_lb)")
 
 # to see that we can do no better than an objective cost of 1188.75.
 
-# However, we have additional power flow constraints to satisfy.
+# (Direct substitution works because a
+# [quadratic function](https://en.wikipedia.org/wiki/Quadratic_function#Graph_of_the_univariate_function)
+# of a single variable ``x`` with positive coefficients is strictly increasing for all ``x \geq 0``.)
+
 # In fact, we can get a quick but even better estimate from the direct observation that the
 # real power generated must meet or exceed the real power demand.
 
@@ -123,9 +138,11 @@ optimize!(model)
 objval_better_lb = round(objective_value(model); digits = 2)
 println("Objective value (better lower bound): $(objval_better_lb)")
 
+# However, there are additional power flow constraints that must be satisfied.
+
 # Power must flow from one or more generation nodes through the transmission lines
 # and end up at a demand node. The state variables of our steady-state alternating current (AC)
-# electrical network are *complex-valued* voltage variables ``V_1, \ldots, V_9``. Voltages capture both a magnitude and phase 
+# electrical network are *complex-valued* voltage variables ``V_1, \ldots, V_N``. Voltages capture both a magnitude and phase 
 # of the node's electrical state in relation to the rest of the system.
 # An AC power system also extends the notion of resistance in wires found in a direct current (DC) circuit to a 
 # complex quantity, known as the *impedance*, of each transmission line.
@@ -134,11 +151,11 @@ println("Objective value (better lower bound): $(objval_better_lb)")
 # through a line is proportional to the difference in voltages on each end of the line
 # multiplied by the admittance.
 
-# ## Data
+# ## Network data
 
 # Let's assemble the data we need for writing the complex power flow constraints. 
 # The data for the problem consists of a list of the real and imaginary parts of the line impedance.
-# We obtain from the `case9mod` MATPOWER test case `branch data` the following data table:
+# We obtain the following data table from the `branch data` section of the `case9mod` MATPOWER format file:
 df_br = DataFrames.DataFrame([
     (1, 4, 0.0, 0.0576, 0.0),
     (4, 5, 0.017, 0.092, 0.158),
@@ -184,7 +201,7 @@ Y =
 # ## JuMP model
 
 # Now we're ready to write the complex power flow constraints we need
-# to more accurately the electricity system.
+# to more accurately model the electricity system.
 
 # We'll introduce a number of constraints that model both the physics and operational requirements.
 
@@ -211,7 +228,9 @@ Q_G = imag(S_G);
 # The loads in this model are assumed to be fixed and of constant-power type:
 @constraint(model, S_D .== S_Demand)
 
-# We need the system state variables, complex nodal voltages:
+# **Operational state** 
+
+# We need  complex nodal voltages (the system state variables):
 @variable(model, V[1:N] in ComplexPlane(), start = 1.0 + 0.0im)
 
 # Operational constraints for maintaining voltage magnitude levels:
@@ -253,14 +272,14 @@ S_Node = LinearAlgebra.diagm(V) * conj(I_Node)
     335
 )
 
+# We're finally ready to solve our AC-OPF problem.
 optimize!(model)
 Test.@test isapprox(objective_value(model), 3087.84, atol = 1e-2)  #src
 solution_summary(model)
-println(
-    "Objective value (feasible solution): $(round(objective_value(model), digits=2))",
-)
+objval_solution = round(objective_value(model); digits = 2)
+println("Objective value (feasible solution): $(objval_solution))")
 
-# We can see the voltage state variable solution:
+# We can see the solution's voltages:
 DataFrames.DataFrame(;
     Bus = 1:N,
     Magnitude = round.(abs.(value.(V)), digits = 2),
@@ -268,7 +287,7 @@ DataFrames.DataFrame(;
 )
 
 # ## Relaxations and better objective bounds
-# The IPOPT solver uses an interior-point algorithm. It has local optimality guarantees, but is unable to certify
+# The Ipopt solver uses an interior-point algorithm. It has local optimality guarantees, but is unable to certify
 # whether the solution is globally optimal. The solution we found is indeed globally optimal.
 # The work to verify this has been done in Bukhsh et al. (2013) and Krasko and Rebennack (2017),
 # and different solvers (such as Gurobi, SCIP and GLOMIQO) are also able to verify this. 
@@ -278,9 +297,9 @@ println("Objective value (better lower bound): $(objval_better_lb)")
 
 # ## References and further resources
 
-# **Krasko**, Vitaliy, and Steffen **Rebennack**.
+# **Krasko**, V., & S. **Rebennack**.
 # [_Chapter 15: Global Optimization: Optimal Power Flow Problem._](https://doi.org/10.1137/1.9781611974683.ch15)
-# In Advances and Trends in Optimization with Engineering Applications, 187–205. MOS-SIAM Series on Optimization.
+# In Advances and Trends in Optimization with Engineering Applications, 187—205. MOS-SIAM Series on Optimization.
 # Society for Industrial and Applied Mathematics, 2017. 
 
 # **Bukhsh**, W. A., Grothey, A., McKinnon, K. I., & Trodden, P. A.
@@ -291,14 +310,19 @@ println("Objective value (better lower bound): $(objval_better_lb)")
 # from the
 # [Test Case Archive of Optimal Power Flow (OPF) Problems with Local Optima](https://www.maths.ed.ac.uk/optenergy/LocalOpt/).
 
+# R. D. Zimmerman, C. E. Murillo-Sanchez, & R. J. Thomas, 
+# [_MATPOWER: Steady-State Operations, Planning and Analysis Tools for Power Systems
+# Research and Education,_](https://doi.org/10.1109/TPWRS.2010.2051168)
+# IEEE Transactions on Power Systems, 26(1), 12-19 (2011).
+
 # **MATPOWER data format**:
-# [MATPOWER manual](https://matpower.org/docs/MATPOWER-manual.pdf): see especially Appendix B "Data File Format" and Table B-3.
+# [MATPOWER manual](https://matpower.org/docs/MATPOWER-manual.pdf); see especially Appendix B "Data File Format" and Table B-3.
 
 # **PowerModels.jl**:
-# The Julia/JuMP package [PowerModels.jl](https://lanl-ansi.github.io/PowerModels.jl/stable/) provides
+# the Julia/JuMP package [PowerModels.jl](https://lanl-ansi.github.io/PowerModels.jl/stable/) provides
 # an interface to a wide range of power flow formulations along with utilities for working with detailed network data.
 
-# **IPOPT solver**:
+# **Ipopt solver**:
 # Wächter, A., Biegler, L. 
 # [_On the implementation of an interior-point filter line-search algorithm for large-scale nonlinear programming._](https://doi.org/10.1007/s10107-004-0559-y)
-# Math. Program. 106, 25–57 (2006).
+# Math. Program. 106, 25—57 (2006).
