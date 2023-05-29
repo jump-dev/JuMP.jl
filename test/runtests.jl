a4cf9e0329d1d432f480e06e36ae6f394905996c
odow <o.dowson@gmail.com>
2023-05-29 14:08:01 +1200

    MathOptInterface#master

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@
 #############################################################################
 
 import Pkg
-Pkg.pkg"add MathOptInterface#od/nlp-expr"
+Pkg.pkg"add MathOptInterface#master"
 
 import JuMP
 import Test
