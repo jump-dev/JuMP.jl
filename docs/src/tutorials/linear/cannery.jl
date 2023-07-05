ae4dca11cc13f0c4aa7549c76d15d7b1cfdc9c3a
Cory Simon <cory.simon@oregonstate.edu>
2023-07-05 14:31:17 -0400

    Update cannery.jl
    define decision variables in cannery tutorial

--- docs/src/tutorials/linear/cannery.jl
+++ docs/src/tutorials/linear/cannery.jl
@@ -31,8 +31,10 @@ import Test  #src
 # Each production plant ``p`` has a capacity, ``c_p``, and each market ``m``
 # has a demand ``d_m``. The distance from plant to market is ``d_{p,m}``.
 
-# With a little effort, we can formulate our problem as the following linear
-# program:
+# We wish to find the distribution plan ``x_{p,m}`` (the quantity of cans
+# shipped from plant `p` to market `m`) for ``p \in P`` and ``m \in M``  
+# that minimizes the shipping costs. We can formulate our problem as the
+# following linear program:
 # ```math
 # \begin{aligned}
 # \min & \sum\limits_{p \in P}\sum\limits_{m \in M} d_{p,m} x_{p,m} \\
