3cbb874c7c66385f420ecbabe4c06fbdb21f2757
Joaquim Dias Garcia <joaquimdgarcia@gmail.com>
2023-11-29 17:31:15 -0500

    Fix type instability in `set_objective_coefficient` (#3590)

--- src/objective.jl
+++ src/objective.jl
@@ -261,29 +261,41 @@ function set_objective_coefficient(
     if _nlp_objective_function(model) !== nothing
         error("A nonlinear objective is already set in the model")
     end
-    coeff = convert(T, coeff)::T
-    obj_fct_type = objective_function_type(model)
-    if obj_fct_type == GenericVariableRef{T}
-        # Promote the objective function to be an affine expression.
-        current_obj = objective_function(model)
-        if index(current_obj) == index(variable)
-            set_objective_function(model, coeff * variable)
-        else
-            set_objective_function(
-                model,
-                add_to_expression!(coeff * variable, current_obj),
-            )
-        end
-    elseif obj_fct_type == GenericAffExpr{T,GenericVariableRef{T}} ||
-           obj_fct_type == GenericQuadExpr{T,GenericVariableRef{T}}
-        MOI.modify(
-            backend(model),
-            MOI.ObjectiveFunction{moi_function_type(obj_fct_type)}(),
-            MOI.ScalarCoefficientChange(index(variable), coeff),
-        )
-    else
-        error("Objective function type not supported: $(obj_fct_type)")
-    end
+    coeff_t = convert(T, coeff)::T
+    F = objective_function_type(model)
+    _set_objective_coefficient(model, variable, coeff_t, F)
     model.is_model_dirty = true
+    return
+end
+
+function _set_objective_coefficient(
+    model::GenericModel{T},
+    variable::GenericVariableRef{T},
+    coeff::T,
+    ::Type{GenericVariableRef{T}},
+) where {T}
+    current_obj = objective_function(model)
+    if index(current_obj) == index(variable)
+        set_objective_function(model, coeff * variable)
+    else
+        set_objective_function(
+            model,
+            add_to_expression!(coeff * variable, current_obj),
+        )
+    end
+    return
+end
+
+function _set_objective_coefficient(
+    model::GenericModel{T},
+    variable::GenericVariableRef{T},
+    coeff::T,
+    ::Type{F},
+) where {T,F}
+    MOI.modify(
+        backend(model),
+        MOI.ObjectiveFunction{moi_function_type(F)}(),
+        MOI.ScalarCoefficientChange(index(variable), coeff),
+    )
     return
 end
