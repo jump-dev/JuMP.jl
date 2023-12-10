9dada50020747a7e1c922da10264d61da9dc0716
odow <o.dowson@gmail.com>
2023-12-11 12:41:42 +1300

    [docs] clean up and clarify macro docstrings

--- src/macros/@expression.jl
+++ src/macros/@expression.jl
@@ -4,37 +4,44 @@
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 """
-    @expression(args...)
+    @expression(model::GenericModel, expression)
+    @expression(model::GenericModel, [index_sets...], expression)
+    @expression(model::GenericModel, name, expression)
+    @expression(model::GenericModel, name[index_sets...], expression)
 
-Efficiently builds a linear or quadratic expression but does not add to model
-immediately. Instead, returns the expression which can then be inserted in other
-constraints.
+Efficiently builds and returns an expression.
+
+The `name` argument is optional. If index sets are passed, a container is built
+and the expression may depend on the indices of the index ssets.
+
+## Keyword arguments
+
+ * `container = :Auto`: force the container type by passing `container = Array`,
+   `container = DenseAxisArray`, `container = SparseAxisArray`, or any another
+   container type which is supported by a JuMP extension.
 
 ## Example
 
-```jldoctest expression_docstring
+```jldoctest
 julia> model = Model();
 
 julia> @variable(model, x[1:5]);
 
-julia> @variable(model, y);
-
-julia> @variable(model, z);
-
 julia> @expression(model, shared, sum(i * x[i] for i in 1:5))
 x[1] + 2 x[2] + 3 x[3] + 4 x[4] + 5 x[5]
 
-julia> @constraint(model, shared + y >= 5)
-x[1] + 2 x[2] + 3 x[3] + 4 x[4] + 5 x[5] + y ≥ 5
-
-julia> @constraint(model, shared + z <= 10)
-x[1] + 2 x[2] + 3 x[3] + 4 x[4] + 5 x[5] + z ≤ 10
+julia> shared
+x[1] + 2 x[2] + 3 x[3] + 4 x[4] + 5 x[5]
 ```
 
-The `ref` accepts index sets in the same way as `@variable`, and those indices
-can be used in the construction of the expressions:
+In the same way as [`@variable`](@ref), the second argument may define index
+sets, and those indices can be used in the construction of the expressions:
 
-```jldoctest expression_docstring
+```jldoctest
+julia> model = Model();
+
+julia> @variable(model, x[1:3]);
+
 julia> @expression(model, expr[i = 1:3], i * sum(x[j] for j in 1:3))
 3-element Vector{AffExpr}:
  x[1] + x[2] + x[3]
@@ -44,7 +51,11 @@ julia> @expression(model, expr[i = 1:3], i * sum(x[j] for j in 1:3))
 
 Anonymous syntax is also supported:
 
-```jldoctest expression_docstring
+```jldoctest
+julia> model = Model();
+
+julia> @variable(model, x[1:3]);
+
 julia> expr = @expression(model, [i in 1:3], i * sum(x[j] for j in 1:3))
 3-element Vector{AffExpr}:
  x[1] + x[2] + x[3]
