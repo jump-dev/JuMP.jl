173155c8dad3a8b876876d0ae375f4fa20c5e89b
Oscar Dowson <odow@users.noreply.github.com>
2024-03-01 07:49:40 +1300

    Update test/test_complex.jl

--- test/test_complex.jl
+++ test/test_complex.jl
@@ -293,7 +293,7 @@ function test_HermitianPSDCone_general_matrix_error()
 end
 
 function test_complex_generic_number_type()
-    for T in (Float32, Float64, Rational{BitInt}, BigFloat)
+    for T in (Float32, Float64, Rational{BigInt}, BigFloat)
         model = GenericModel{T}()
         @variable(model, x in ComplexPlane())
         @test x isa GenericAffExpr{T,GenericVariableRef{T}}
