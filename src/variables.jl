9ef84d533fe25c12fc3d74d713296d32d96730e8
Joaquim Garcia <joaquimgarcia@local.psrservices.net>
2024-03-22 20:13:41 -0300

    fix docs

--- src/variables.jl
+++ src/variables.jl
@@ -2759,7 +2759,7 @@ julia> @variable(model, x[1:2]);
 julia> @constraint(model, con, 2x[1]^2 + 3 * x[1] * x[2] + x[2] <= 2)
 con : 2 x[1]² + 3 x[1]*x[2] + x[2] ≤ 2
 
-julia> set_normalized_coefficient([con, con], [x[1], x[1]], [x[1], x[2]], [4, 5])
+julia> set_normalized_coefficients([con, con], [x[1], x[1]], [x[1], x[2]], [4, 5])
 
 julia> con
 con : 4 x[1]² + 5 x[1]*x[2] + x[2] ≤ 2
