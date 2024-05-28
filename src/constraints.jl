76cafbda3d13ed76f204a3d783e670e022a72692
odow <o.dowson@gmail.com>
2024-05-28 14:16:25 +1200

    Update

--- src/constraints.jl
+++ src/constraints.jl
@@ -1282,7 +1282,7 @@ end
 Return the dual value of constraint `con_ref` associated with result index
 `result` of the most-recent solution returned by the solver.
 
-Use [`has_dual`](@ref) to check if a result exists before asking for values.
+Use [`has_duals`](@ref) to check if a result exists before asking for values.
 
 See also: [`result_count`](@ref), [`shadow_price`](@ref).
 """
