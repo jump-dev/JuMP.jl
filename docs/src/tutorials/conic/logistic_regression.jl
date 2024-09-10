dbd9ed46d216a6b42da8c188ef73530cb7ea72c8
odow <o.dowson@gmail.com>
2024-09-11 08:04:42 +1200

    Update

--- docs/src/tutorials/conic/logistic_regression.jl
+++ docs/src/tutorials/conic/logistic_regression.jl
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #src
 # SOFTWARE.                                                                      #src
 
-# # Logistic regression
+# # Example: logistic regression
 
 # **This tutorial was originally contributed by François Pacaud.**
 
