b016cdd1f839c5037ce13dc544e4a40b860c9d0a
Oscar Dowson <odow@users.noreply.github.com>
2024-11-06 15:00:59 +1300

    Simplify how we copy docstrings for MOI enums (#3872)

--- src/JuMP.jl
+++ src/JuMP.jl
@@ -1307,38 +1307,14 @@ include("print.jl")
 # `MOI.OPTIMAL`. This piece of code re-exports them from JuMP so that users can
 # use: `MOI.OPTIMAL`, `JuMP.OPTIMAL`, or `using JuMP; OPTIMAL`.
 
-function _eval_instances_in_jump(moi_enum)
-    for enum in instances(moi_enum)
-        name = Symbol(enum)
-        # `eval` the instance as `const name = MOI.name`.
-        eval(Expr(:const, Expr(:(=), name, enum)))
-        # Documentation is not copied for a `const` expression of an object, so
-        # we need to manually set the docstring in this `@__MODULE__`.
-        docstr = Docs.docstr(Docs.Binding(MOI, name))
-        d = Docs.DocStr(
-            docstr.text,
-            nothing,
-            Dict{Symbol,Any}(
-                :typesig => Union{},
-                :module => @__MODULE__,
-                :linenumber => @__LINE__,
-                :binding => getfield(@__MODULE__, name),
-                :path => @__FILE__,
-            ),
-        )
-        Docs.doc!(@__MODULE__, Docs.Binding(@__MODULE__, name), d, Union{})
+for sym in [:ResultStatusCode, :TerminationStatusCode, :OptimizationSense]
+    @eval const $sym = MOI.$sym
+    for enum in instances(getfield(MOI, sym))
+        @eval const $(Symbol(enum)) = $enum
+        docstr = Docs.docstr(Docs.Binding(MOI, Symbol(enum)))
+        Docs.doc!(@__MODULE__, Docs.Binding(@__MODULE__, Symbol(enum)), docstr)
     end
-    return
 end
-
-const ResultStatusCode = MOI.ResultStatusCode
-_eval_instances_in_jump(MOI.ResultStatusCode)
-
-const TerminationStatusCode = MOI.TerminationStatusCode
-_eval_instances_in_jump(MOI.TerminationStatusCode)
-
-const OptimizationSense = MOI.OptimizationSense
-_eval_instances_in_jump(MOI.OptimizationSense)
 
 # JuMP exports everything except internal symbols, which are defined as those
 # whose name starts with an underscore. Macros whose names start with
