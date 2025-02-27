93520fb7995559ca9d9cb1738602aa75540a1f91
odow <o.dowson@gmail.com>
2025-02-28 11:00:36 +1300

    Update

--- src/JuMP.jl
+++ src/JuMP.jl
@@ -1357,22 +1357,21 @@ export MOI
 
 import PrecompileTools
 
+function _optimizer_for_precompile_tools()
+    return MOI.Utilities.MockOptimizer(MOI.Utilities.Model{Float64}())
+end
+
 PrecompileTools.@setup_workload begin
-    _optimizer_ = () -> MOI.Utilities.MockOptimizer(
-        MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
-    )
     PrecompileTools.@compile_workload begin
+        # We don't care about this particular optimizer, but it still
+        # exercises generic code paths that calls like
+        # Model(HiGHS.Optimizer) also need.
+        model = Model(_optimizer_for_precompile_tools)
+        optimize!(model)
         # Because lots of the work is done by macros, and macros are expanded
         # at lowering time, not much of this would get precompiled without `@eval`
         @eval begin
-            # We don't care about this particular optimizer, but it still
-            # exercises generic code paths that calls like
-            # Model(HiGHS.Optimizer) also need.
-            Model(_optimizer_)
-            # Use an empty model to build, which is a common use-case, and
-            # doesn't bake in Utilities.MockOptimizer.
             model = Model()
-            set_silent(model)
             @variables(model, begin
                 x1 >= 0
                 0 <= x2 <= 1
@@ -1405,8 +1404,6 @@ PrecompileTools.@setup_workload begin
             @objective(model, Min, x1)
             @objective(model, Max, a)
             @objective(model, Min, b)
-            set_optimizer(model, _optimizer_)
-            optimize!(model)
         end
     end
 end
