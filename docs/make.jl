dc7d9181b11e40352592ea55b22297ba1f293aae
Oscar Dowson <o.dowson@gmail.com>
2026-01-16 16:13:39 +1300

    UPdate

--- docs/make.jl
+++ docs/make.jl
@@ -154,6 +154,58 @@ function _add_edit_url(filename, url)
 end
 
 # ==============================================================================
+#  Add tutorial overview
+# ==============================================================================
+
+package_to_headers = Dict{String,Vector{String}}()
+
+for (root, dirs, files) in walkdir(joinpath(@__DIR__, "src", "tutorials"))
+    for file in filter(f -> endswith(f, ".jl") | endswith(f, ".md"), files)
+        contents = read(joinpath(root, file), String)
+        if startswith(contents, "```@meta")
+            continue  # Already literated
+        end
+        m = match(r"\n# # (.+)\n", contents)
+        if m === nothing
+            m = match(r"^# (.+)\n", contents)
+        end
+        title = m[1]
+        anchor = "[$title](@ref)"
+        if occursin("@id", title)
+            m = match(r"\[(.+)\]\(@id (.+)\)", title)
+            anchor = "[$(m[1])](@ref $(m[2]))"
+        end
+        for m in eachmatch(r"\nimport ([a-z][A-Z]+)\n"i, contents; overlap = true)
+            push!(get!(package_to_headers, m[1], String[]), anchor)
+        end
+    end
+end
+
+open(joinpath(@__DIR__, "src", "tutorials", "overview.md"), "w") do io
+    println(
+        io,
+        """
+        # Tutorial overview
+
+        To make it easier to find relevant tutorials, this page lists all the tutorials
+        by the packages they import.
+
+        For example, to find tutorials that solve nonlinear programs, look at the
+        [Ipopt.jl](@ref tutorial_Ipopt) section.
+        """,
+    )
+    for (pkg, tutorials) in sort!(collect(package_to_headers))
+        if pkg == "JuMP"
+            continue
+        end
+        println(io, "## [$(pkg).jl](@id tutorial_$pkg)")
+        println(io, join(unique(sort!(tutorials)), ", "))
+        println(io)
+    end
+    return
+end
+
+# ==============================================================================
 #  Add solver README
 # ==============================================================================
 
