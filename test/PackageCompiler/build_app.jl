2006ef5e86d9128ccd48e1b44cee5cbc67e47cdb
Oscar Dowson <o.dowson@gmail.com>
2026-03-19 12:51:06 +1300

    Add a CI test for PackageCompiler.jl

---
+++ test/PackageCompiler/build_app.jl
@@ -0,0 +1,31 @@
+#  Copyright 2017, Iain Dunning, Joey Huchette, Miles Lubin, and contributors
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#############################################################################
+# JuMP
+# An algebraic modeling language for Julia
+# See https://github.com/jump-dev/JuMP.jl
+#############################################################################
+
+using Test
+
+import PackageCompiler
+
+PackageCompiler.create_app("MyApp", "build"; force = true)
+
+@testset "test --solver=ipopt" begin
+    output = sprint() do io
+        return run(pipeline(`build/bin/MyApp --solver=highs`; stdout = io))
+    end
+    @test occursin("HiGHS", output)
+    @test occursin("OPTIMAL", output)
+end
+
+@testset "test --solver=ipopt" begin
+    output = sprint() do io
+        return run(pipeline(`build/bin/MyApp --solver=ipopt`; stdout = io))
+    end
+    @test occursin("Ipopt", output)
+    @test occursin("LOCALLY_SOLVED", output)
+end
