faac6fd54e4f47bcf3679d98a2f48b3a2360542f
Oscar Dowson <odow@users.noreply.github.com>
2026-06-19 14:48:27 +1200

    Fix failing test on Julia nightly (#4173)

--- test/test_macros.jl
+++ test/test_macros.jl
@@ -1635,7 +1635,9 @@ end
 function test_set_string_name_Symmetric()
     model = Model()
     x = @variable(model, [1:2, 1:2], Symmetric, set_string_name = false)
-    @test name.(x) == fill("", 2, 2)
+    for xi in x
+        @test isempty(name(xi))
+    end
     @test sprint.(show, x) == ["_[1]" "_[2]"; "_[2]" "_[3]"]
     return
 end
@@ -1644,7 +1646,9 @@ function test_set_string_name_PSDCone()
     model = Model()
     set_string_names_on_creation(model, false)
     x = @variable(model, [1:2, 1:2] in PSDCone())
-    @test name.(x) == fill("", 2, 2)
+    for xi in x
+        @test isempty(name(xi))
+    end
     @test sprint.(show, x) == ["_[1]" "_[2]"; "_[2]" "_[3]"]
     return
 end
