af5d201ffed58ae0158d46896d28795ab4655f98
Oscar Dowson <o.dowson@gmail.com>
2026-07-29 10:57:25 +1200

    Fix docs and formatting

--- src/nlp_expr.jl
+++ src/nlp_expr.jl
@@ -77,7 +77,8 @@ julia> f = GenericNonlinearExpr{VariableRef}(
 sin(x) ^ 2
 ```
 """
-mutable struct GenericNonlinearExpr{V<:AbstractVariableRef} <: AbstractJuMPScalar
+mutable struct GenericNonlinearExpr{V<:AbstractVariableRef} <:
+               AbstractJuMPScalar
     head::Symbol
     args::Vector{Any}
 
