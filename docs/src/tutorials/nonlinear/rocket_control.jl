ecfec6dd5a86268a7c08a98ce01a4ee92a96002a
Oscar Dowson <odow@users.noreply.github.com>
2026-09-08 20:50:50 +1200

    [docs] fix use of T_max in rocket control tutorial (#4247)

--- docs/src/tutorials/nonlinear/rocket_control.jl
+++ docs/src/tutorials/nonlinear/rocket_control.jl
@@ -86,7 +86,7 @@ D_c = 0.5 * 620 * m_0 / g_0  # Drag scaling
 u_t_max = 3.5 * g_0 * m_0    # Maximum thrust
 T_max = 0.2                  # Number of seconds
 T = 1_000                    # Number of time steps
-Δt = 0.2 / T;                # Time per discretized step
+Δt = T_max / T;              # Time per discretized step
 
 # ## JuMP formulation
 
